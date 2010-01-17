markjdb/freebsd

commit 3c0ac1086bb9d2b012fb6325ae7fb27c3baceaf3

Add tcsetsid(3).
The entire world seems to use the non-standard TIOCSCTTY ioctl to make a
TTY a controlling terminal of a session. Even though tcsetsid(3) is also
non-standard, I think it's a lot better to use in our own source code,
mainly because it's similar to tcsetpgrp(), tcgetpgrp() and tcgetsid().

I stole the idea from QNX. They do it the other way around; their
TIOCSCTTY is just a wrapper around tcsetsid(). tcsetsid() then calls
into an IPC framework.


git-svn-id: svn://svn.freebsd.org/base/head@191882 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

lib/libutil/login_tty.c
@@ -37,17 +37,21 @@ static char sccsid[] = "@(#)login_tty.c	8.1 (Berkeley) 6/4/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
-#include <sys/ioctl.h>
 
 #include <libutil.h>
 #include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
 
 int
 login_tty(int fd)
 {
-	(void) setsid();
-	if (ioctl(fd, TIOCSCTTY, (char *)NULL) == -1)
+	pid_t s;
+
+	s = setsid();
+	if (s == -1)
+		return (-1);
+	if (tcsetsid(fd, s) == -1)
 		return (-1);
 	(void) dup2(fd, 0);
 	(void) dup2(fd, 1);

lib/libc/gen/Makefile.inc
@@ -70,8 +70,8 @@ MAN+=	alarm.3 arc4random.3 \
 	siginterrupt.3 signal.3 sigsetops.3 sleep.3 \
 	statvfs.3 stringlist.3 \
 	strtofflags.3 sysconf.3 sysctl.3 syslog.3 tcgetpgrp.3 tcgetsid.3 \
-	tcsendbreak.3 tcsetattr.3 tcsetpgrp.3 time.3 times.3 timezone.3 \
-	ttyname.3 tzset.3 ualarm.3 ucontext.3 ulimit.3 uname.3 \
+	tcsendbreak.3 tcsetattr.3 tcsetpgrp.3 tcsetsid.3 time.3 times.3 \
+	timezone.3 ttyname.3 tzset.3 ualarm.3 ucontext.3 ulimit.3 uname.3 \
 	unvis.3 usleep.3 utime.3 valloc.3 vis.3 wordexp.3
 
 MLINKS+=arc4random.3 arc4random_addrandom.3 arc4random.3 arc4random_stir.3 \

lib/libc/gen/Symbol.map
@@ -363,6 +363,7 @@ FBSD_1.1 {
 	posix_spawnattr_setsigmask;
 	posix_spawnp;
 	tcgetsid;
+	tcsetsid;
 };
 
 FBSDprivate_1.0 {

lib/libc/gen/tcsetsid.3
@@ -1,4 +1,4 @@
-.\" Copyright (c) 2008 David Xu <davidxu@freebsd.org>
+.\" Copyright (c) 2009 Ed Schouten <ed@FreeBSD.org>
 .\" All rights reserved.
 .\"
 .\" Redistribution and use in source and binary forms, with or without
@@ -24,28 +24,40 @@
 .\"
 .\" $FreeBSD$
 .\"
-.Dd April 15, 2008
-.Dt TCGETSID 3
+.Dd May 4, 2009
+.Dt TCSETSID 3
 .Os
 .Sh NAME
-.Nm tcgetsid
-.Nd get session ID associated with a controlling terminal
+.Nm tcsetsid
+.Nd set session ID associated with a controlling terminal
 .Sh LIBRARY
 .Lb libc
 .Sh SYNOPSIS
 .In sys/types.h
 .In termios.h
-.Ft pid_t
-.Fn tcgetsid "int fd"
+.Ft int
+.Fn tcsetsid "int fd" "pid_t pid"
 .Sh DESCRIPTION
 The
-.Fn tcgetsid
-function returns the process group ID of the session leader for a
-controlling terminal specified by
+.Fn tcsetsid
+function sets associates a session identified by
+.Fa pid
+with a controlling terminal specified by
 .Fa fd .
+.Pp
+This implementation only allows the controlling terminal to be changed
+by the session leader itself.
+This implies that
+.Fa pid
+always has to be equal to the process ID.
+.Pp
+It is unsupported to associate with a terminal that already has an
+associated session.
+Conversely, it is also unsupported to associate to a terminal when
+the session is already associated with a different terminal.
 .Sh ERRORS
 If an error occurs,
-.Fn tcgetsid
+.Fn tcsetsid
 returns -1 and the global variable
 .Va errno
 is set to indicate the error, as follows:
@@ -55,18 +67,26 @@ The
 .Fa fd
 argument is not a valid file descriptor.
 .It Bq Er ENOTTY
-The calling process does not have a controlling terminal or the
-underlying terminal device represented by
+The file descriptor represented by
 .Fa fd
-is not the controlling terminal.
+is not a terminal.
+.It Bq Er EINVAL
+The
+.Fa pid
+argument is not equal to the session ID of the calling process.
+.It Bq Er EPERM
+The calling process is not a session leader.
+.It Bq Er EPERM
+The session already has an associated terminal or the terminal already
+has an associated session.
 .El
 .Sh SEE ALSO
 .Xr getsid 2 ,
 .Xr setsid 2 ,
 .Xr tcgetpgrp 3 ,
-.Xr tcsetsid 3
-.Sh STANDARDS
-The
-.Fn tcgetsid
-function conforms to
-.St -xpg4.2 .
+.Xr tcgetsid 3
+.Sh HISTORY
+A
+.Fn tcsetsid
+function first appeared in QNX.
+It does not comply to any standard.

lib/libc/gen/termios.c
@@ -110,6 +110,18 @@ tcgetsid(int fd)
 	return ((pid_t)s);
 }
 
+int
+tcsetsid(int fd, pid_t pid)
+{
+
+	if (pid != getsid(0)) {
+		errno = EINVAL;
+		return (-1);
+	}
+
+	return (_ioctl(fd, TIOCSCTTY, NULL));
+}
+
 speed_t
 cfgetospeed(t)
 	const struct termios *t;

sys/sys/termios.h
@@ -272,8 +272,9 @@ int	tcsendbreak(int, int);
 #if __POSIX_VISIBLE >= 200112 || __BSD_VISIBLE
 pid_t	tcgetsid(int);
 #endif
-
 #if __BSD_VISIBLE
+int	tcsetsid(int, pid_t);
+
 void	cfmakeraw(struct termios *);
 int	cfsetspeed(struct termios *, speed_t);
 #endif

commit 0c06706e01699772eaf51aa59cf3bee423ded9cf

Fix a regression that was introduced in r191882.
I changed login_tty() to only work when the application is not a session
leader yet. This works fine for applications in the base system, but it
turns out various applications call this function after daemonizing,
which means they already use their own session.

If setsid() fails, just call tcsetsid() on the current session.
tcsetsid() will already perform proper security checks.

Reported by:	Oliver Lehmann
MFC after:	1 week

lib/libutil/login_tty.c
@@ -50,7 +50,7 @@ login_tty(int fd)
 
 	s = setsid();
 	if (s == -1)
-		return (-1);
+		s = getsid(0);
 	if (tcsetsid(fd, s) == -1)
 		return (-1);
 	(void) dup2(fd, 0);