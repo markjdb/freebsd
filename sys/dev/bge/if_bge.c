markjdb/freebsd

commit 01a5112b1171eeadb580cfaaede7d04bcb21a6f9

Rename dev.bge.%d.msi_disable to dev.bge.%d.msi which matches enable/disable and default it to on.
Suggested by:	jhb


git-svn-id: https://svn.freebsd.org/base/head@230337 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/bge/if_bge.c
@@ -2745,7 +2745,7 @@ bge_can_use_msi(struct bge_softc *sc)
 {
 	int can_use_msi = 0;
 
-	if (sc->bge_msi_disable != 0)
+	if (sc->bge_msi != 0)
 		return (0);
 
 	/* Disable MSI for polling(4). */
@@ -5630,11 +5630,11 @@ bge_add_sysctls(struct bge_softc *sc)
 	    "Number of fragmented TX buffers of a frame allowed before "
 	    "forced collapsing");
 
-	sc->bge_msi_disable = 0;
-	snprintf(tn, sizeof(tn), "dev.bge.%d.msi_disable", unit);
-	TUNABLE_INT_FETCH(tn, &sc->bge_msi_disable);
-	SYSCTL_ADD_INT(ctx, children, OID_AUTO, "msi_disable",
-	    CTLFLAG_RD, &sc->bge_msi_disable, 0, "Disable MSI");
+	sc->bge_msi = 1;
+	snprintf(tn, sizeof(tn), "dev.bge.%d.msi", unit);
+	TUNABLE_INT_FETCH(tn, &sc->bge_msi);
+	SYSCTL_ADD_INT(ctx, children, OID_AUTO, "msi",
+	    CTLFLAG_RD, &sc->bge_msi, 0, "Enable MSI");
 
 	/*
 	 * It seems all Broadcom controllers have a bug that can generate UDP

sys/dev/bge/if_bgereg.h
@@ -2864,7 +2864,7 @@ struct bge_softc {
 	int			bge_timer;
 	int			bge_forced_collapse;
 	int			bge_forced_udpcsum;
-	int			bge_msi_disable;
+	int			bge_msi;
 	int			bge_csum_features;
 	struct callout		bge_stat_ch;
 	uint32_t		bge_rx_discards;

commit 06af20479a5ebdac44a4a6fb8d7175950aa94e1e

Oops, fix logic error introduced in r230337.

sys/dev/bge/if_bge.c
@@ -2745,7 +2745,7 @@ bge_can_use_msi(struct bge_softc *sc)
 {
 	int can_use_msi = 0;
 
-	if (sc->bge_msi != 0)
+	if (sc->bge_msi == 0)
 		return (0);
 
 	/* Disable MSI for polling(4). */