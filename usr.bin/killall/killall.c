markjdb/freebsd

commit 6cca7ae0ed5ef84dcba96672e6a3cf780a765e85

Add a -I flag which requests confirmation before action, like what is done in pkill(1).
MFC after:	2 weeks


git-svn-id: https://svn.freebsd.org/base/head@237618 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

usr.bin/killall/killall.c
@@ -53,7 +53,7 @@ static void __dead2
 usage(void)
 {
 
-	fprintf(stderr, "usage: killall [-delmsvz] [-help] [-j jail]\n");
+	fprintf(stderr, "usage: killall [-delmsvz] [-help] [-I] [-j jail]\n");
 	fprintf(stderr,
 	    "               [-u user] [-t tty] [-c cmd] [-SIGNAL] [cmd]...\n");
 	fprintf(stderr, "At least one option or argument to specify processes must be given.\n");
@@ -95,8 +95,9 @@ main(int ac, char **av)
 	struct passwd	*pw;
 	regex_t		rgx;
 	regmatch_t	pmatch;
-	int		i, j;
+	int		i, j, ch;
 	char		buf[256];
+	char		first;
 	char		*user = NULL;
 	char		*tty = NULL;
 	char		*cmd = NULL;
@@ -104,6 +105,7 @@ main(int ac, char **av)
 	int		sflag = 0;
 	int		dflag = 0;
 	int		eflag = 0;
+	int		Iflag = 0;
 	int		jflag = 0;
 	int		mflag = 0;
 	int		zflag = 0;
@@ -141,6 +143,9 @@ main(int ac, char **av)
 		if (**av == '-') {
 			++*av;
 			switch (**av) {
+			case 'I':
+				Iflag = 1;
+				break;
 			case 'j':
 				++*av;
 				if (**av == '\0') {
@@ -381,6 +386,16 @@ main(int ac, char **av)
 			}
 			if (matched)
 				break;
+		}
+		if (matched != 0 && Iflag) {
+			printf("Send signal %d to %s (pid %d uid %d)? ",
+				sig, thiscmd, thispid, thisuid);
+			fflush(stdout);
+			first = ch = getchar();
+			while (ch != '\n' && ch != EOF)
+				ch = getchar();
+			if (first != 'y' && first != 'Y')
+				matched = 0;
 		}
 		if (matched == 0)
 			continue;

usr.bin/killall/killall.1
@@ -24,7 +24,7 @@
 .\"
 .\" $FreeBSD$
 .\"
-.Dd December 25, 2009
+.Dd June 27, 2012
 .Dt KILLALL 1
 .Os
 .Sh NAME
@@ -34,6 +34,7 @@
 .Nm
 .Op Fl delmsvz
 .Op Fl help
+.Op Fl I
 .Op Fl j Ar jail
 .Op Fl u Ar user
 .Op Fl t Ar tty
@@ -71,6 +72,9 @@ processes specified with the
 option.
 .It Fl help
 Give a help on the command usage and exit.
+.It Fl I
+Request confirmation before attempting to signal each
+process.
 .It Fl l
 List the names of the available signals and exit, like in
 .Xr kill 1 .

commit 00fc5b144deaf768a5cfb8120fe0401f9afb6bd0

fix a regression introduced in r237618 that would result in killall confusing killall -INT with killall -I (interactive confirmation) which resulted in the wrong signal (TERM) being delivered to the process(s).
Discussed with:	delphij
MFC after:	2 weeks

usr.bin/killall/killall.c
@@ -144,9 +144,6 @@ main(int ac, char **av)
 		if (**av == '-') {
 			++*av;
 			switch (**av) {
-			case 'I':
-				Iflag = 1;
-				break;
 			case 'j':
 				++*av;
 				if (**av == '\0') {
@@ -213,6 +210,15 @@ main(int ac, char **av)
 			case 'z':
 				zflag++;
 				break;
+			case 'I':
+				/*
+				 * NB: do not confuse -INT with -I
+				 */
+				if (strncmp(*av, "INT", 3) != 0) {
+					Iflag = 1;
+					break;
+				}
+				/* FALLTHROUGH */
 			default:
 				if (isalpha((unsigned char)**av)) {
 					if (strncasecmp(*av, "SIG", 3) == 0)