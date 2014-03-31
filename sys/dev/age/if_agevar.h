markjdb/freebsd

commit 55502a9564bb090316388ffbe3c454c530b77cf1

- Added TSO support - Updated firmware to latest release (v3.4.8) to fix TSO + jumbo frame lockup - Added MSI (hw.bce.msi_enable) and TSO (hw.bce.tso_enable) sysctls - Fixed kernel panic when MSI is used and module is unloaded - Added several new debug routines - Removed slack space for RX/TX chains since it only covers sloppy coding - Fixed a potential problem when programming jumbo MTU size in hardware - Various other comment changes
MFC after:	4 weeks


git-svn-id: svn://svn.freebsd.org/base/head@169632 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/bce/if_bcereg.h
@@ -48,20 +48,23 @@
 #include <sys/sysctl.h>
 #include <sys/queue.h>
 
+#include <net/bpf.h>
+#include <net/ethernet.h>
 #include <net/if.h>
 #include <net/if_arp.h>
-#include <net/ethernet.h>
 #include <net/if_dl.h>
 #include <net/if_media.h>
-
-#include <net/bpf.h>
 
 #include <net/if_types.h>
 #include <net/if_vlan_var.h>
 
 #include <netinet/in_systm.h>
 #include <netinet/in.h>
+#include <netinet/if_ether.h>
 #include <netinet/ip.h>
+#include <netinet/ip6.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
 
 #include <machine/bus.h>
 #include <machine/resource.h>
@@ -4474,7 +4477,6 @@ struct l2_fhdr {
 #define TOTAL_TX_BD (TOTAL_TX_BD_PER_PAGE * TX_PAGES)
 #define USABLE_TX_BD (USABLE_TX_BD_PER_PAGE * TX_PAGES)
 #define MAX_TX_BD (TOTAL_TX_BD - 1)
-#define BCE_TX_SLACK_SPACE 16
 
 #define RX_PAGES	2
 #define TOTAL_RX_BD_PER_PAGE  (BCM_PAGE_SIZE / sizeof(struct rx_bd))
@@ -4482,7 +4484,6 @@ struct l2_fhdr {
 #define TOTAL_RX_BD (TOTAL_RX_BD_PER_PAGE * RX_PAGES)
 #define USABLE_RX_BD (USABLE_RX_BD_PER_PAGE * RX_PAGES)
 #define MAX_RX_BD (TOTAL_RX_BD - 1)
-#define BCE_RX_SLACK_SPACE (MAX_RX_BD - 8)
 
 #define NEXT_TX_BD(x) (((x) & USABLE_TX_BD_PER_PAGE) ==	\
 		(USABLE_TX_BD_PER_PAGE - 1)) ?					  	\
@@ -4604,7 +4605,10 @@ struct fw_info {
 
 #define BCE_TX_TIMEOUT					5
 
-#define BCE_MAX_SEGMENTS				8
+#define BCE_MAX_SEGMENTS				32
+#define BCE_TSO_MAX_SIZE				65536
+#define BCE_TSO_MAX_SEG_SIZE			4096
+
 #define BCE_DMA_ALIGN		 			8
 #define BCE_DMA_BOUNDARY				0
 
@@ -4663,12 +4667,12 @@ struct bce_softc
 	struct ifnet		*bce_ifp;			/* Interface info */
 	device_t			bce_dev;			/* Parent device handle */
 	u_int8_t			bce_unit;			/* Interface number */
-	struct resource		*bce_res;			/* Device resource handle */
+	struct resource		*bce_res_mem;  		/* Device resource handle */
 	struct ifmedia		bce_ifmedia;		/* TBI media info */
 	bus_space_tag_t		bce_btag;			/* Device bus tag */
 	bus_space_handle_t	bce_bhandle;		/* Device bus handle */
 	vm_offset_t			bce_vhandle;		/* Device virtual memory handle */
-	struct resource		*bce_irq;			/* IRQ Resource Handle */
+	struct resource		*bce_res_irq;		/* IRQ Resource Handle */
 	struct mtx			bce_mtx;			/* Mutex */
 	void				*bce_intrhand;		/* Interrupt handler */
 
@@ -4683,14 +4687,14 @@ struct bce_softc
 #define BCE_NO_WOL_FLAG			0x08
 #define BCE_USING_DAC_FLAG		0x10
 #define BCE_USING_MSI_FLAG 		0x20
-#define BCE_MFW_ENABLE_FLAG		0x40
+#define BCE_MFW_ENABLE_FLAG		0x40		/* Management F/W is enabled */
 
 	/* PHY specific flags. */
 	u32					bce_phy_flags;
-#define BCE_PHY_SERDES_FLAG					1
-#define BCE_PHY_CRC_FIX_FLAG				2
-#define BCE_PHY_PARALLEL_DETECT_FLAG		4
-#define BCE_PHY_2_5G_CAPABLE_FLAG			8
+#define BCE_PHY_SERDES_FLAG					0x001
+#define BCE_PHY_CRC_FIX_FLAG				0x002
+#define BCE_PHY_PARALLEL_DETECT_FLAG		0x004
+#define BCE_PHY_2_5G_CAPABLE_FLAG			0x008
 #define BCE_PHY_INT_MODE_MASK_FLAG			0x300
 #define BCE_PHY_INT_MODE_AUTO_POLLING_FLAG	0x100
 #define BCE_PHY_INT_MODE_LINK_READY_FLAG	0x200
@@ -4816,7 +4820,9 @@ struct bce_softc
 
 	/* Track the number of rx_bd and tx_bd's in use. */
 	u16 free_rx_bd;
+	u16 max_rx_bd;
 	u16 used_tx_bd;
+	u16 max_tx_bd;
 
 	/* Provides access to hardware statistics through sysctl. */
 	u64 stat_IfHCInOctets;
@@ -4890,11 +4896,15 @@ struct bce_softc
 	u32 tx_interrupts;
 
 	u32	rx_low_watermark;			/* Lowest number of rx_bd's free. */
+	u32 rx_empty_count;				/* Number of times the RX chain was empty. */
 	u32 tx_hi_watermark;			/* Greatest number of tx_bd's used. */
+	u32	tx_full_count;				/* Number of times the TX chain was full. */
 	u32	mbuf_alloc_failed;			/* Mbuf allocation failure counter. */
 	u32 l2fhdr_status_errors;
 	u32 unexpected_attentions;
 	u32	lost_status_block_updates;
+
+	u32	requested_tso_frames;		/* Number of TSO frames enqueued. */
 #endif
 };
 

commit 137073478e662765923f4f98e2f55d5230dee2c2

Increase the number of TX DMA segments from 32 to 35.  It turned out 32 is not enough to support a full sized TSO packet. While I'm here fix a long standing bug introduced in r169632 in bce(4) where it didn't include L2 header length of TSO packet in the maximum DMA segment size calculation.
In collaboration with:	rmacklem
MFC after:		2 weeks

sys/dev/age/if_agevar.h
@@ -42,7 +42,7 @@
 
 #define	AGE_TSO_MAXSEGSIZE	4096
 #define	AGE_TSO_MAXSIZE		(65535 + sizeof(struct ether_vlan_header))
-#define	AGE_MAXTXSEGS		32
+#define	AGE_MAXTXSEGS		35
 #define	AGE_RX_BUF_ALIGN	8
 #ifndef __NO_STRICT_ALIGNMENT
 #define	AGE_RX_BUF_SIZE		(MCLBYTES - AGE_RX_BUF_ALIGN)	

sys/dev/alc/if_alcvar.h
@@ -42,7 +42,7 @@
 
 #define	ALC_TSO_MAXSEGSIZE	4096
 #define	ALC_TSO_MAXSIZE		(65535 + sizeof(struct ether_vlan_header))
-#define	ALC_MAXTXSEGS		32
+#define	ALC_MAXTXSEGS		35
 
 #define	ALC_ADDR_LO(x)		((uint64_t) (x) & 0xFFFFFFFF)
 #define	ALC_ADDR_HI(x)		((uint64_t) (x) >> 32)

sys/dev/ale/if_alevar.h
@@ -40,7 +40,7 @@
 
 #define	ALE_TSO_MAXSEGSIZE	4096
 #define	ALE_TSO_MAXSIZE		(65535 + sizeof(struct ether_vlan_header))
-#define	ALE_MAXTXSEGS		32
+#define	ALE_MAXTXSEGS		35
 
 #define	ALE_ADDR_LO(x)		((uint64_t) (x) & 0xFFFFFFFF)
 #define	ALE_ADDR_HI(x)		((uint64_t) (x) >> 32)

sys/dev/bce/if_bcereg.h
@@ -6309,8 +6309,8 @@ struct fw_info {
 
 #define BCE_TX_TIMEOUT		5
 
-#define BCE_MAX_SEGMENTS	32
-#define BCE_TSO_MAX_SIZE	65536
+#define BCE_MAX_SEGMENTS	35
+#define BCE_TSO_MAX_SIZE	(65535 + sizeof(struct ether_vlan_header))
 #define BCE_TSO_MAX_SEG_SIZE	4096
 
 #define BCE_DMA_ALIGN		8

sys/dev/bge/if_bgereg.h
@@ -2852,7 +2852,7 @@ struct bge_gib {
  */
 
 #define	BGE_NSEG_JUMBO	4
-#define	BGE_NSEG_NEW	32
+#define	BGE_NSEG_NEW	35
 #define	BGE_TSOSEG_SZ	4096
 
 /* Maximum DMA address for controllers that have 40bit DMA address bug. */

sys/dev/fxp/if_fxpreg.h
@@ -250,7 +250,7 @@ struct fxp_cb_ucode {
 /*
  * Number of DMA segments in a TxCB.
  */
-#define FXP_NTXSEG	32
+#define FXP_NTXSEG	35
 
 struct fxp_tbd {
 	uint32_t tb_addr;

sys/dev/jme/if_jmevar.h
@@ -49,7 +49,7 @@
 #define	JME_RX_RING_ALIGN	16
 #define	JME_TSO_MAXSEGSIZE	4096
 #define	JME_TSO_MAXSIZE		(65535 + sizeof(struct ether_vlan_header))
-#define	JME_MAXTXSEGS		32
+#define	JME_MAXTXSEGS		35
 #define	JME_RX_BUF_ALIGN	sizeof(uint64_t)
 #define	JME_SSB_ALIGN		16
 

sys/dev/msk/if_mskreg.h
@@ -2338,7 +2338,7 @@ struct msk_stat_desc {
 #endif
 #define	MSK_RX_BUF_ALIGN	8
 #define MSK_JUMBO_RX_RING_CNT	MSK_RX_RING_CNT
-#define MSK_MAXTXSEGS		32
+#define MSK_MAXTXSEGS		35
 #define	MSK_TSO_MAXSGSIZE	4096
 #define	MSK_TSO_MAXSIZE		(65535 + sizeof(struct ether_vlan_header))
 

sys/dev/nfe/if_nfereg.h
@@ -40,7 +40,7 @@
 	(NFE_JUMBO_FRAMELEN - NFE_RX_HEADERS)
 #define	NFE_MIN_FRAMELEN	(ETHER_MIN_LEN - ETHER_CRC_LEN)
 
-#define	NFE_MAX_SCATTER		32
+#define	NFE_MAX_SCATTER		35
 #define	NFE_TSO_MAXSGSIZE	4096
 #define	NFE_TSO_MAXSIZE		(65535 + sizeof(struct ether_vlan_header))
 

sys/dev/sge/if_sgereg.h
@@ -284,7 +284,7 @@ struct sge_desc {
 #define	SGE_RX_RING_CNT		256 /* [8, 1024] */
 #define	SGE_TX_RING_CNT		256 /* [8, 8192] */
 #define	SGE_DESC_ALIGN		16
-#define	SGE_MAXTXSEGS		32
+#define	SGE_MAXTXSEGS		35
 #define	SGE_TSO_MAXSIZE		(65535 + sizeof(struct ether_vlan_header))
 #define	SGE_TSO_MAXSEGSIZE	4096
 #define	SGE_RX_BUF_ALIGN	sizeof(uint64_t)

sys/pci/if_rlreg.h
@@ -776,7 +776,7 @@ struct rl_stats {
 #define	RL_TX_DESC_CNT		RL_8169_TX_DESC_CNT
 #define	RL_RX_DESC_CNT		RL_8169_RX_DESC_CNT
 #define	RL_RX_JUMBO_DESC_CNT	RL_RX_DESC_CNT
-#define	RL_NTXSEGS		32
+#define	RL_NTXSEGS		35
 
 #define	RL_RING_ALIGN		256
 #define	RL_DUMP_ALIGN		64