markjdb/freebsd

commit 11296d89889da8d48cb0270cf01604ad401be06b

INTRNG: Rework handling with resources. Partially revert r301453.  - Read interrupt properties at bus enumeration time and store    it into global mapping table.  - At bus_activate_resource() time, given mapping entry is resolved and    connected to real interrupt source. A copy of mapping entry is attached    to given resource.  - At bus_setup_intr() time, mapping entry stored in resource is used    for delivery of requested interrupt configuration.  - For MSI/MSIX interrupts, mapping entry is created within    pci_alloc_msi()/pci_alloc_msix() call.  - For legacy PCI interrupts, mapping entry must be created within    pcib_route_interrupt() by pcib driver itself.
Reviewed by: nwhitehorn, andrew
Differential Revision: https://reviews.freebsd.org/D7493


git-svn-id: svn+ssh://svn.freebsd.org/base/head@304459 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/mips/mips/nexus.c
@@ -64,6 +64,7 @@ __FBSDID("$FreeBSD$");
 #endif
 
 #ifdef FDT
+#include <dev/ofw/ofw_bus_subr.h>
 #include <dev/ofw/openfirm.h>
 #include "ofw_bus_if.h"
 #endif
@@ -429,6 +430,10 @@ nexus_activate_resource(device_t bus, device_t child, int type, int rid,
 		}
 		rman_set_virtual(r, vaddr);
 		rman_set_bushandle(r, (bus_space_handle_t)(uintptr_t)vaddr);
+	} else if (type == SYS_RES_IRQ) {
+#ifdef INTRNG
+		intr_activate_irq(child, r);
+#endif
 	}
 
 	return (rman_activate_resource(r));
@@ -448,6 +453,10 @@ nexus_deactivate_resource(device_t bus, device_t child, int type, int rid,
 		bus_space_unmap(rman_get_bustag(r), vaddr, psize);
 		rman_set_virtual(r, NULL);
 		rman_set_bushandle(r, 0);
+	} else if (type == SYS_RES_IRQ) {
+#ifdef INTRNG
+		intr_deactivate_irq(child, r);
+#endif
 	}
 
 	return (rman_deactivate_resource(r));
@@ -523,12 +532,18 @@ static int
 nexus_ofw_map_intr(device_t dev, device_t child, phandle_t iparent, int icells,
     pcell_t *intr)
 {
+	u_int irq;
+	struct intr_map_data_fdt *fdt_data;
+	size_t len;
 
-#ifdef INTRNG
-	return (INTR_IRQ_INVALID);
-#else
-	return (intr_fdt_map_irq(iparent, intr, icells));
-#endif
+	len = sizeof(*fdt_data) + icells * sizeof(pcell_t);
+	fdt_data = (struct intr_map_data_fdt *)intr_alloc_map_data(
+	    INTR_MAP_DATA_FDT, len, M_WAITOK | M_ZERO);
+	fdt_data->iparent = iparent;
+	fdt_data->ncells = icells;
+	memcpy(fdt_data->cells, intr, icells * sizeof(pcell_t));
+	irq = intr_map_irq(NULL, iparent, (struct intr_map_data *)fdt_data);
+	return (irq);
 }
 #endif
 #endif /* INTRNG */

sys/arm/arm/nexus.c
@@ -64,6 +64,7 @@ __FBSDID("$FreeBSD$");
 
 #ifdef FDT
 #include <machine/fdt.h>
+#include <dev/ofw/ofw_bus_subr.h>
 #include "ofw_bus_if.h"
 #endif
 
@@ -379,6 +380,11 @@ nexus_activate_resource(device_t bus, device_t child, int type, int rid,
 #endif
 		rman_set_virtual(r, (void *)vaddr);
 		rman_set_bushandle(r, vaddr);
+		return (0);
+	} else if (type == SYS_RES_IRQ) {
+#ifdef INTRNG
+		intr_activate_irq(child, r);
+#endif
 	}
 	return (0);
 }
@@ -390,17 +396,23 @@ nexus_deactivate_resource(device_t bus, device_t child, int type, int rid,
 	bus_size_t psize;
 	bus_space_handle_t vaddr;
 
-	psize = (bus_size_t)rman_get_size(r);
-	vaddr = rman_get_bushandle(r);
+	if (type == SYS_RES_MEMORY || type == SYS_RES_IOPORT) {
+		psize = (bus_size_t)rman_get_size(r);
+		vaddr = rman_get_bushandle(r);
 
-	if (vaddr != 0) {
+		if (vaddr != 0) {
 #ifdef FDT
-		bus_space_unmap(fdtbus_bs_tag, vaddr, psize);
+			bus_space_unmap(fdtbus_bs_tag, vaddr, psize);
 #else
-		pmap_unmapdev((vm_offset_t)vaddr, (vm_size_t)psize);
+			pmap_unmapdev((vm_offset_t)vaddr, (vm_size_t)psize);
 #endif
-		rman_set_virtual(r, NULL);
-		rman_set_bushandle(r, 0);
+			rman_set_virtual(r, NULL);
+			rman_set_bushandle(r, 0);
+		}
+	} else if (type == SYS_RES_IRQ) {
+#ifdef INTRNG
+		intr_deactivate_irq(child, r);
+#endif
 	}
 
 	return (rman_deactivate_resource(r));
@@ -411,11 +423,22 @@ static int
 nexus_ofw_map_intr(device_t dev, device_t child, phandle_t iparent, int icells,
     pcell_t *intr)
 {
-
-#ifdef INTRNG
-	return (INTR_IRQ_INVALID);
-#else
+#ifndef INTRNG
 	return (intr_fdt_map_irq(iparent, intr, icells));
-#endif
+#else
+	u_int irq;
+	struct intr_map_data_fdt *fdt_data;
+	size_t len;
+
+	len = sizeof(*fdt_data) + icells * sizeof(pcell_t);
+	fdt_data = (struct intr_map_data_fdt *)intr_alloc_map_data(
+	    INTR_MAP_DATA_FDT, len, M_WAITOK | M_ZERO);
+	fdt_data->iparent = iparent;
+	fdt_data->ncells = icells;
+	memcpy(fdt_data->cells, intr, icells * sizeof(pcell_t));
+	irq = intr_map_irq(NULL, iparent, (struct intr_map_data *)fdt_data);
+	return (irq);
+#endif /* INTRNG */
 }
-#endif
+#endif /* FDT */
+

sys/arm64/arm64/nexus.c
@@ -65,6 +65,7 @@ __FBSDID("$FreeBSD$");
 #include <machine/intr.h>
 
 #ifdef FDT
+#include <dev/ofw/ofw_bus_subr.h>
 #include <dev/ofw/openfirm.h>
 #include "ofw_bus_if.h"
 #endif
@@ -345,6 +346,8 @@ nexus_activate_resource(device_t bus, device_t child, int type, int rid,
 		rman_set_bustag(r, &memmap_bus);
 		rman_set_virtual(r, (void *)vaddr);
 		rman_set_bushandle(r, vaddr);
+	} else if (type == SYS_RES_IRQ) {
+		intr_activate_irq(child, r);
 	}
 	return (0);
 }
@@ -378,13 +381,17 @@ nexus_deactivate_resource(device_t bus, device_t child, int type, int rid,
 	bus_size_t psize;
 	bus_space_handle_t vaddr;
 
-	psize = (bus_size_t)rman_get_size(r);
-	vaddr = rman_get_bushandle(r);
+	if (type == SYS_RES_MEMORY || type == SYS_RES_IOPORT) {
+		psize = (bus_size_t)rman_get_size(r);
+		vaddr = rman_get_bushandle(r);
 
-	if (vaddr != 0) {
-		bus_space_unmap(&memmap_bus, vaddr, psize);
-		rman_set_virtual(r, NULL);
-		rman_set_bushandle(r, 0);
+		if (vaddr != 0) {
+			bus_space_unmap(&memmap_bus, vaddr, psize);
+			rman_set_virtual(r, NULL);
+			rman_set_bushandle(r, 0);
+		}
+	} else if (type == SYS_RES_IRQ) {
+		intr_deactivate_irq(child, r);
 	}
 
 	return (rman_deactivate_resource(r));
@@ -431,8 +438,18 @@ static int
 nexus_ofw_map_intr(device_t dev, device_t child, phandle_t iparent, int icells,
     pcell_t *intr)
 {
+	u_int irq;
+	struct intr_map_data_fdt *fdt_data;
+	size_t len;
 
-	return (INTR_IRQ_INVALID);
+	len = sizeof(*fdt_data) + icells * sizeof(pcell_t);
+	fdt_data = (struct intr_map_data_fdt *)intr_alloc_map_data(
+	    INTR_MAP_DATA_FDT, len, M_WAITOK | M_ZERO);
+	fdt_data->iparent = iparent;
+	fdt_data->ncells = icells;
+	memcpy(fdt_data->cells, intr, icells * sizeof(pcell_t));
+	irq = intr_map_irq(NULL, iparent, (struct intr_map_data *)fdt_data);
+	return (irq);
 }
 #endif
 

sys/sys/intr.h
@@ -29,10 +29,37 @@
 
 #ifndef _SYS_INTR_H_
 #define _SYS_INTR_H_
+#ifndef INTRNG
+#error Need INTRNG for this file
+#endif
 
 #include <sys/systm.h>
 
 #define	INTR_IRQ_INVALID	0xFFFFFFFF
+
+enum intr_map_data_type {
+	INTR_MAP_DATA_ACPI = 0,
+	INTR_MAP_DATA_FDT,
+	INTR_MAP_DATA_GPIO,
+	INTR_MAP_DATA_MSI,
+	
+	/* Placeholders for platform specific types */
+	INTR_MAP_DATA_PLAT_1 = 1000,
+	INTR_MAP_DATA_PLAT_2,
+	INTR_MAP_DATA_PLAT_3,
+	INTR_MAP_DATA_PLAT_4,
+	INTR_MAP_DATA_PLAT_5,
+};
+
+struct intr_map_data {
+	size_t			len;
+	enum intr_map_data_type	type;
+};
+
+struct intr_map_data_msi {
+	struct intr_map_data	hdr;
+	struct intr_irqsrc 	*isrc;
+};
 
 #ifdef notyet
 #define	INTR_SOLO	INTR_MD1
@@ -88,10 +115,9 @@ struct intr_pic *intr_pic_add_handler(device_t, struct intr_pic *,
 extern device_t intr_irq_root_dev;
 
 /* Intr interface for BUS. */
-int intr_map_irq(device_t, intptr_t, struct intr_map_data *, u_int *);
 
-int intr_alloc_irq(device_t, struct resource *);
-int intr_release_irq(device_t, struct resource *);
+int intr_activate_irq(device_t, struct resource *);
+int intr_deactivate_irq(device_t, struct resource *);
 
 int intr_setup_irq(device_t, struct resource *, driver_filter_t, driver_intr_t,
     void *, int, void **);
@@ -99,6 +125,13 @@ int intr_teardown_irq(device_t, struct resource *, void *);
 
 int intr_describe_irq(device_t, struct resource *, void *, const char *);
 int intr_child_irq_handler(struct intr_pic *, uintptr_t);
+
+/* Intr resources  mapping. */
+struct intr_map_data *intr_alloc_map_data(enum intr_map_data_type, size_t, int);
+void intr_free_intr_map_data(struct intr_map_data *);
+u_int intr_map_irq(device_t, intptr_t, struct intr_map_data *);
+void intr_unmap_irq(u_int );
+u_int intr_map_clone_irq(u_int );
 
 /* MSI/MSI-X handling */
 int intr_msi_register(device_t, intptr_t);

sys/arm/nvidia/tegra_lic.c
@@ -88,12 +88,12 @@ struct tegra_lic_sc {
 };
 
 static int
-tegra_lic_alloc_intr(device_t dev, struct intr_irqsrc *isrc,
+tegra_lic_activate_intr(device_t dev, struct intr_irqsrc *isrc,
     struct resource *res, struct intr_map_data *data)
 {
 	struct tegra_lic_sc *sc = device_get_softc(dev);
 
-	return (PIC_ALLOC_INTR(sc->parent, isrc, res, data));
+	return (PIC_ACTIVATE_INTR(sc->parent, isrc, res, data));
 }
 
 static void
@@ -122,12 +122,12 @@ tegra_lic_map_intr(device_t dev, struct intr_map_data *data,
 }
 
 static int
-tegra_lic_release_intr(device_t dev, struct intr_irqsrc *isrc,
+tegra_lic_deactivate_intr(device_t dev, struct intr_irqsrc *isrc,
     struct resource *res, struct intr_map_data *data)
 {
 	struct tegra_lic_sc *sc = device_get_softc(dev);
 
-	return (PIC_RELEASE_INTR(sc->parent, isrc, res, data));
+	return (PIC_DEACTIVATE_INTR(sc->parent, isrc, res, data));
 }
 
 static int
@@ -266,11 +266,11 @@ static device_method_t tegra_lic_methods[] = {
 	DEVMETHOD(device_detach,	tegra_lic_detach),
 
 	/* Interrupt controller interface */
-	DEVMETHOD(pic_alloc_intr,	tegra_lic_alloc_intr),
+	DEVMETHOD(pic_activate_intr,	tegra_lic_activate_intr),
 	DEVMETHOD(pic_disable_intr,	tegra_lic_disable_intr),
 	DEVMETHOD(pic_enable_intr,	tegra_lic_enable_intr),
 	DEVMETHOD(pic_map_intr,		tegra_lic_map_intr),
-	DEVMETHOD(pic_release_intr,	tegra_lic_release_intr),
+	DEVMETHOD(pic_deactivate_intr,	tegra_lic_deactivate_intr),
 	DEVMETHOD(pic_setup_intr,	tegra_lic_setup_intr),
 	DEVMETHOD(pic_teardown_intr,	tegra_lic_teardown_intr),
 	DEVMETHOD(pic_pre_ithread,	tegra_lic_pre_ithread),

sys/arm/ti/omap4/omap4_wugen.c
@@ -57,12 +57,12 @@ struct omap4_wugen_sc {
 };
 
 static int
-omap4_wugen_alloc_intr(device_t dev, struct intr_irqsrc *isrc,
+omap4_wugen_activate_intr(device_t dev, struct intr_irqsrc *isrc,
     struct resource *res, struct intr_map_data *data)
 {
 	struct omap4_wugen_sc *sc = device_get_softc(dev);
 
-	return (PIC_ALLOC_INTR(sc->sc_parent, isrc, res, data));
+	return (PIC_ACTIVATE_INTR(sc->sc_parent, isrc, res, data));
 }
 
 static void
@@ -91,12 +91,12 @@ omap4_wugen_map_intr(device_t dev, struct intr_map_data *data,
 }
 
 static int
-omap4_wugen_release_intr(device_t dev, struct intr_irqsrc *isrc,
+omap4_wugen_deactivate_intr(device_t dev, struct intr_irqsrc *isrc,
     struct resource *res, struct intr_map_data *data)
 {
 	struct omap4_wugen_sc *sc = device_get_softc(dev);
 
-	return (PIC_RELEASE_INTR(sc->sc_parent, isrc, res, data));
+	return (PIC_DEACTIVATE_INTR(sc->sc_parent, isrc, res, data));
 }
 
 static int
@@ -227,11 +227,11 @@ static device_method_t omap4_wugen_methods[] = {
 	DEVMETHOD(device_detach,	omap4_wugen_detach),
 
 	/* Interrupt controller interface */
-	DEVMETHOD(pic_alloc_intr,	omap4_wugen_alloc_intr),
+	DEVMETHOD(pic_activate_intr,	omap4_wugen_activate_intr),
 	DEVMETHOD(pic_disable_intr,	omap4_wugen_disable_intr),
 	DEVMETHOD(pic_enable_intr,	omap4_wugen_enable_intr),
 	DEVMETHOD(pic_map_intr,		omap4_wugen_map_intr),
-	DEVMETHOD(pic_release_intr,	omap4_wugen_release_intr),
+	DEVMETHOD(pic_deactivate_intr,	omap4_wugen_deactivate_intr),
 	DEVMETHOD(pic_setup_intr,	omap4_wugen_setup_intr),
 	DEVMETHOD(pic_teardown_intr,	omap4_wugen_teardown_intr),
 	DEVMETHOD(pic_pre_ithread,	omap4_wugen_pre_ithread),

sys/dev/fdt/simplebus.c
@@ -251,9 +251,7 @@ simplebus_setup_dinfo(device_t dev, phandle_t node,
 
 	resource_list_init(&ndi->rl);
 	ofw_bus_reg_to_rl(dev, node, sc->acells, sc->scells, &ndi->rl);
-#ifndef INTRNG
 	ofw_bus_intr_to_rl(dev, node, &ndi->rl, NULL);
-#endif
 
 	return (ndi);
 }

sys/dev/gpio/gpiobus.c
@@ -31,7 +31,9 @@ __FBSDID("$FreeBSD$");
 #include <sys/systm.h>
 #include <sys/bus.h>
 #include <sys/gpio.h>
+#ifdef INTRNG
 #include <sys/intr.h>
+#endif
 #include <sys/kernel.h>
 #include <sys/malloc.h>
 #include <sys/module.h>
@@ -79,43 +81,26 @@ static int gpiobus_pin_toggle(device_t, device_t, uint32_t);
  * data will be moved into struct resource.
  */
 #ifdef INTRNG
-static void
-gpio_destruct_map_data(struct intr_map_data *map_data)
-{
-
-	KASSERT(map_data->type == INTR_MAP_DATA_GPIO,
-	    ("%s: bad map_data type %d", __func__, map_data->type));
-
-	free(map_data, M_DEVBUF);
-}
 
 struct resource *
 gpio_alloc_intr_resource(device_t consumer_dev, int *rid, u_int alloc_flags,
     gpio_pin_t pin, uint32_t intr_mode)
 {
-	int rv;
 	u_int irq;
 	struct intr_map_data_gpio *gpio_data;
 	struct resource *res;
 
-	gpio_data = malloc(sizeof(*gpio_data), M_DEVBUF, M_WAITOK | M_ZERO);
-	gpio_data->hdr.type = INTR_MAP_DATA_GPIO;
-	gpio_data->hdr.destruct = gpio_destruct_map_data;
+	gpio_data = (struct intr_map_data_gpio *)intr_alloc_map_data(
+	    INTR_MAP_DATA_GPIO, sizeof(*gpio_data), M_WAITOK | M_ZERO);
 	gpio_data->gpio_pin_num = pin->pin;
 	gpio_data->gpio_pin_flags = pin->flags;
 	gpio_data->gpio_intr_mode = intr_mode;
 
-	rv = intr_map_irq(pin->dev, 0, (struct intr_map_data *)gpio_data,
-	    &irq);
-	if (rv != 0) {
-		gpio_destruct_map_data((struct intr_map_data *)gpio_data);
-		return (NULL);
-	}
-
+	irq = intr_map_irq(pin->dev, 0, (struct intr_map_data *)gpio_data);
 	res = bus_alloc_resource(consumer_dev, SYS_RES_IRQ, rid, irq, irq, 1,
 	    alloc_flags);
 	if (res == NULL) {
-		gpio_destruct_map_data((struct intr_map_data *)gpio_data);
+		intr_free_intr_map_data((struct intr_map_data *)gpio_data);
 		return (NULL);
 	}
 	rman_set_virtual(res, gpio_data);

commit 16023cd11fb59288be0402af1b976d8df612ebda

Fix MIPS INTRNG (both FDT and non-FDT) behaviour broken by r304459
More changes to MIPS may be required, as commented in D7692, but this
revision aims to restore MIPS INTRNG functionality so we can move on
with working interrupts.

Reported by:	yamori813@yahoo.co.jp
Tested by:	mizhka (on BCM), sgalabov (on Mediatek)
Reviewed by:	adrian, nwhitehorn (older version)
Sponsored by:	Smartcom - Bulgaria AD
Differential Revision:	https://reviews.freebsd.org/D7692

sys/mips/include/intr.h
@@ -63,6 +63,8 @@ void cpu_establish_hardintr(const char *, driver_filter_t *, driver_intr_t *,
     void *, int, int, void **);
 void cpu_establish_softintr(const char *, driver_filter_t *, void (*)(void*),
     void *, int, int, void **);
+int cpu_create_intr_map(int);
+struct resource *cpu_get_irq_resource(int);
 /* MIPS interrupt C entry point */
 void cpu_intr(struct trapframe *);
 

sys/mips/mips/mips_pic.c
@@ -71,6 +71,11 @@ __FBSDID("$FreeBSD$");
 
 static int mips_pic_intr(void *);
 
+struct intr_map_data_mips_pic {
+	struct intr_map_data	hdr;
+	u_int			irq;
+};
+
 struct mips_pic_irqsrc {
 	struct intr_irqsrc	isrc;
 	struct resource		*res;
@@ -304,24 +309,37 @@ static int
 mips_pic_map_intr(device_t dev, struct intr_map_data *data,
     struct intr_irqsrc **isrcp)
 {
-#ifdef FDT
-	struct intr_map_data_fdt *daf;
 	struct mips_pic_softc *sc;
-
-	if (data->type != INTR_MAP_DATA_FDT)
-		return (ENOTSUP);
+	int res;
 
 	sc = device_get_softc(dev);
-	daf = (struct intr_map_data_fdt *)data;
+	res = 0;
+#ifdef FDT
+	if (data->type == INTR_MAP_DATA_FDT) {
+		struct intr_map_data_fdt *daf;
 
-	if (daf->ncells != 1 || daf->cells[0] >= sc->nirqs)
-		return (EINVAL);
+		daf = (struct intr_map_data_fdt *)data;
 
-	*isrcp = PIC_INTR_ISRC(sc, daf->cells[0]);
-	return (0);
-#else
-	return (ENOTSUP);
+		if (daf->ncells != 1 || daf->cells[0] >= sc->nirqs)
+			return (EINVAL);
+
+		*isrcp = PIC_INTR_ISRC(sc, daf->cells[0]);
+	} else
 #endif
+	if (data->type == INTR_MAP_DATA_PLAT_1) {
+		struct intr_map_data_mips_pic *mpd;
+
+		mpd = (struct intr_map_data_mips_pic *)data;
+
+		if (mpd->irq < 0 || mpd->irq >= sc->nirqs)
+			return (EINVAL);
+
+		*isrcp = PIC_INTR_ISRC(sc, mpd->irq);
+	} else {
+		res = ENOTSUP;
+	}
+
+	return (res);
 }
 
 static void
@@ -383,6 +401,46 @@ cpu_init_interrupts(void)
 {
 }
 
+int
+cpu_create_intr_map(int irq)
+{
+	struct intr_map_data_mips_pic *mips_pic_data;
+	intptr_t iparent;
+	size_t len;
+	u_int new_irq;
+
+	len = sizeof(*mips_pic_data);
+	iparent = pic_xref(pic_sc->pic_dev);
+
+	/* Allocate mips_pic data and fill it in */
+	mips_pic_data = (struct intr_map_data_mips_pic *)intr_alloc_map_data(
+	    INTR_MAP_DATA_PLAT_1, len, M_WAITOK | M_ZERO);
+	mips_pic_data->irq = irq;
+
+	/* Get the new irq number */
+	new_irq = intr_map_irq(pic_sc->pic_dev, iparent,
+	    (struct intr_map_data *)mips_pic_data);
+
+	/* Adjust the resource accordingly */
+	rman_set_start(pic_sc->pic_irqs[irq].res, new_irq);
+	rman_set_end(pic_sc->pic_irqs[irq].res, new_irq);
+
+	/* Activate the new irq */
+	return (intr_activate_irq(pic_sc->pic_dev, pic_sc->pic_irqs[irq].res));
+}
+
+struct resource *
+cpu_get_irq_resource(int irq)
+{
+
+	KASSERT(pic_sc != NULL, ("%s: no pic", __func__));
+
+	if (irq < 0 || irq >= pic_sc->nirqs)
+		panic("%s called for unknown irq %d", __func__, irq);
+
+	return pic_sc->pic_irqs[irq].res;
+}
+
 void
 cpu_establish_hardintr(const char *name, driver_filter_t *filt,
     void (*handler)(void*), void *arg, int irq, int flags, void **cookiep)
@@ -398,6 +456,10 @@ cpu_establish_hardintr(const char *name, driver_filter_t *filt,
 	KASSERT(pic_sc != NULL, ("%s: no pic", __func__));
 
 	irq += NSOFT_IRQS;
+
+	res = cpu_create_intr_map(irq);
+	if (res != 0) panic("Unable to create map for hard IRQ %d", irq);
+
 	res = intr_setup_irq(pic_sc->pic_dev, pic_sc->pic_irqs[irq].res, filt,
 	    handler, arg, flags, cookiep);
 	if (res != 0) panic("Unable to add hard IRQ %d handler", irq);
@@ -414,6 +476,9 @@ cpu_establish_softintr(const char *name, driver_filter_t *filt,
 		panic("%s called for unknown soft intr %d", __func__, irq);
 
 	KASSERT(pic_sc != NULL, ("%s: no pic", __func__));
+
+	res = cpu_create_intr_map(irq);
+	if (res != 0) panic("Unable to create map for soft IRQ %d", irq);
 
 	res = intr_setup_irq(pic_sc->pic_dev, pic_sc->pic_irqs[irq].res, filt,
 	    handler, arg, flags, cookiep);

sys/mips/mips/nexus.c
@@ -432,7 +432,16 @@ nexus_activate_resource(device_t bus, device_t child, int type, int rid,
 		rman_set_bushandle(r, (bus_space_handle_t)(uintptr_t)vaddr);
 	} else if (type == SYS_RES_IRQ) {
 #ifdef INTRNG
+#ifdef FDT
 		intr_activate_irq(child, r);
+#else
+		/*
+		 * INTRNG without FDT needs to have the interrupt properly
+		 * mapped first. cpu_create_intr_map() will do that and
+		 * call intr_activate_irq() at the end.
+		 */
+		cpu_create_intr_map(rman_get_start(r));
+#endif
 #endif
 	}
 
@@ -466,9 +475,13 @@ static int
 nexus_setup_intr(device_t dev, device_t child, struct resource *res, int flags,
     driver_filter_t *filt, driver_intr_t *intr, void *arg, void **cookiep)
 {
-
 #ifdef INTRNG
-	return (intr_setup_irq(child, res, filt, intr, arg, flags, cookiep));
+	struct resource *r = res;
+
+#ifndef FDT
+	r = cpu_get_irq_resource(rman_get_start(r));
+#endif
+	return (intr_setup_irq(child, r, filt, intr, arg, flags, cookiep));
 #else
 	int irq;
 	register_t s;