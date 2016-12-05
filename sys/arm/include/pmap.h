markjdb/freebsd

commit 9745212cdbb5f7d6559ca55c38c0161bd697f172

Implement fake pmap_mapdev_attr() for ARMv6. This function is referenced, but never called from DRM2 code. Also, real behavior of pmap_mapdev_attr() in ARM world is unclear as we don't have any additional attribute for a device memory type.
MFC after: 2 weeks


git-svn-id: https://svn.freebsd.org/base/head@309531 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/arm/include/pmap.h
@@ -53,6 +53,12 @@ void pmap_page_set_memattr(vm_page_t, vm_memattr_t);
 void *pmap_mapdev(vm_paddr_t, vm_size_t);
 void pmap_unmapdev(vm_offset_t, vm_size_t);
 
+static inline void *
+pmap_mapdev_attr(vm_paddr_t addr, vm_size_t size, int attr)
+{
+	panic("%s is not implemented yet!\n", __func__);
+}
+
 struct pcb;
 void pmap_set_pcb_pagedir(pmap_t, struct pcb *);
 

sys/arm/include/pmap-v6.h
@@ -166,7 +166,6 @@ extern ttb_entry_t pmap_kern_ttb; 	/* TTB for kernel pmap */
 void pmap_bootstrap(vm_offset_t);
 void pmap_kenter(vm_offset_t, vm_paddr_t);
 void pmap_kremove(vm_offset_t);
-void *pmap_mapdev_attr(vm_paddr_t, vm_size_t, int);
 boolean_t pmap_page_is_mapped(vm_page_t);
 
 void pmap_tlb_flush(pmap_t, vm_offset_t);

commit 014ea9335659204603968651dd1e666f19e9bf72

Fix build breakage caused by r309531.
Reported by: andrew
MFC after: 2 weeks
X-MFC with: r309531

sys/arm/include/pmap.h
@@ -30,6 +30,7 @@
 #ifndef _MACHINE_PMAP_H_
 #define _MACHINE_PMAP_H_
 
+#include <sys/systm.h>
 #if __ARM_ARCH >= 6
 #include <machine/pmap-v6.h>
 #else