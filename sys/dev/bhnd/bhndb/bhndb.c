markjdb/freebsd

commit 6536c09d39a512ab3a5ef4722d64b6ad8879bc53

bhndb(4): Implement bridge support for the BCM4312 and other PCI_V0 chipsets.
Very early (PCI_V0) Broadcom PCI Wi-Fi chipsets have a few quirks when
compared to later PCI(e) core revisions:

- The standard static BAR0 mapping of the PCI core registers is discontiguous,
  with siba's cfg0 register block mapped distinctly from the other core
  registers.
- No dedicated ChipCommon register mapping is provided; instead, the
  single configurable register window must be used to access both
  ChipCommon and D11 core registers. The D11 core's operational semantics
  guarantee the safety of -- after disabling interrupts -- borrowing
  the single dynamic register window to perform the few ChipCommon
  operations required by a driver.

To support these early PCI devices:

- Allow defining multiple discontiguous BHNDB_REGWIN_T_CORE register
  windows that map a single port/region, and producing bridged resource
  allocations backed by those discontiguous windows.
- Support stealing existing register window allocations to fulfill indirect
  bhnd(4) bus I/O requests within address ranges tagged with
  BHNDB_ALLOC_FULFILL_ON_OVERCOMMIT.
- Fix an inverted test of bhndb_is_pcie_attached() that disabled
  PCI-only clock bring-up required by these devices.

Approved by:	adrian (mentor, implicit)
Sponsored by:	The FreeBSD Foundation


git-svn-id: https://svn.freebsd.org/base/head@326297 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/bhnd/bhndb/bhndb.c
@@ -115,8 +115,8 @@ static int			 bhndb_try_activate_resource(
 
 static inline struct bhndb_dw_alloc *bhndb_io_resource(struct bhndb_softc *sc,
 					bus_addr_t addr, bus_size_t size,
-					bus_size_t *offset);
-
+					bus_size_t *offset, bool *stolen,
+					bus_addr_t *restore);
 
 /**
  * Default bhndb(4) implementation of DEVICE_PROBE().
@@ -270,6 +270,9 @@ bhndb_init_region_cfg(struct bhndb_softc *sc, bhnd_erom_t *erom,
 		for (regw = br->cfg->register_windows;
 		    regw->win_type != BHNDB_REGWIN_T_INVALID; regw++)
 		{
+			const struct bhndb_port_priority	*pp;
+			uint32_t				 alloc_flags;
+
 			/* Only core windows are supported */
 			if (regw->win_type != BHNDB_REGWIN_T_CORE)
 				continue;
@@ -295,6 +298,18 @@ bhndb_init_region_cfg(struct bhndb_softc *sc, bhnd_erom_t *erom,
 			}
 
 			/*
+			 * Apply the register window's region offset, if any.
+			 */
+			if (regw->d.core.offset > size) {
+				device_printf(sc->dev, "invalid register "
+				    "window offset %#jx for region %#jx+%#jx\n",
+				    regw->d.core.offset, addr, size);
+				return (EINVAL);
+			}
+
+			addr += regw->d.core.offset;
+
+			/*
 			 * Always defer to the register window's size.
 			 * 
 			 * If the port size is smaller than the window size,
@@ -307,14 +322,25 @@ bhndb_init_region_cfg(struct bhndb_softc *sc, bhnd_erom_t *erom,
 			 */
 			size = regw->win_size;
 
+			/* Fetch allocation flags from the corresponding port
+			 * priority entry, if any */
+			pp = bhndb_hw_priorty_find_port(table, core,
+			    regw->d.core.port_type, regw->d.core.port,
+			    regw->d.core.region);
+			if (pp != NULL) {
+				alloc_flags = pp->alloc_flags;
+			} else {
+				alloc_flags = 0;
+			}
+
 			/*
 			 * Add to the bus region list.
 			 * 
-			 * The window priority for a statically mapped
-			 * region is always HIGH.
+			 * The window priority for a statically mapped region is
+			 * always HIGH.
 			 */
 			error = bhndb_add_resource_region(br, addr, size,
-			    BHNDB_PRIORITY_HIGH, regw);
+			    BHNDB_PRIORITY_HIGH, 0, regw);
 			if (error)
 				return (error);
 		}
@@ -325,7 +351,6 @@ bhndb_init_region_cfg(struct bhndb_softc *sc, bhnd_erom_t *erom,
 	 * ports defined in the priority table
 	 */
 	for (u_int i = 0; i < ncores; i++) {
-		struct bhndb_region	*region;
 		struct bhnd_core_info	*core;
 		struct bhnd_core_match	 md;
 
@@ -369,13 +394,12 @@ bhndb_init_region_cfg(struct bhndb_softc *sc, bhnd_erom_t *erom,
 			}
 
 			/* Skip ports with an existing static mapping */
-			region = bhndb_find_resource_region(br, addr, size);
-			if (region != NULL && region->static_regwin != NULL)
+			if (bhndb_has_static_region_mapping(br, addr, size))
 				continue;
 
 			/* Define a dynamic region for this port */
 			error = bhndb_add_resource_region(br, addr, size,
-			    pp->priority, NULL);
+			    pp->priority, pp->alloc_flags, NULL);
 			if (error)
 				return (error);
 
@@ -416,22 +440,29 @@ bhndb_init_region_cfg(struct bhndb_softc *sc, bhnd_erom_t *erom,
 		struct bhndb_region	*region;
 		const char		*direct_msg, *type_msg;
 		bhndb_priority_t	 prio, prio_min;
+		uint32_t		 flags;
 
 		prio_min = br->min_prio;
 		device_printf(sc->dev, "min_prio: %d\n", prio_min);
 
 		STAILQ_FOREACH(region, &br->bus_regions, link) {
 			prio = region->priority;
+			flags = region->alloc_flags;
 
 			direct_msg = prio >= prio_min ? "direct" : "indirect";
 			type_msg = region->static_regwin ? "static" : "dynamic";
 	
 			device_printf(sc->dev, "region 0x%llx+0x%llx priority "
-			    "%u %s/%s\n",
+			    "%u %s/%s",
 			    (unsigned long long) region->addr, 
 			    (unsigned long long) region->size,
 			    region->priority,
 			    direct_msg, type_msg);
+
+			if (flags & BHNDB_ALLOC_FULFILL_ON_OVERCOMMIT)
+				printf(" [overcommit]\n");
+			else
+				printf("\n");
 		}
 	}
 
@@ -1605,11 +1636,12 @@ bhndb_deactivate_bhnd_resource(device_t dev, device_t child,
  * in-use region; the first matching region is returned.
  */
 static struct bhndb_dw_alloc *
-bhndb_io_resource_slow(struct bhndb_softc *sc, bus_addr_t addr,
-    bus_size_t size, bus_size_t *offset)
+bhndb_io_resource_slow(struct bhndb_softc *sc, bus_addr_t addr, bus_size_t size,
+    bus_size_t *offset, bool *stolen, bus_addr_t *restore)
 {
 	struct bhndb_resources	*br;
 	struct bhndb_dw_alloc	*dwa;
+	struct bhndb_region	*region;
 
 	BHNDB_LOCK_ASSERT(sc, MA_OWNED);
 
@@ -1638,10 +1670,25 @@ bhndb_io_resource_slow(struct bhndb_softc *sc, bus_addr_t addr,
 		*offset = dwa->win->win_offset;
 		*offset += addr - dwa->target;
 
+		*stolen = false;
 		return (dwa);
 	}
 
-	/* not found */
+	/* No existing dynamic mapping found. We'll need to check for a defined
+	 * region to determine whether we can fulfill this request by
+	 * stealing from an existing allocated register window */
+	region = bhndb_find_resource_region(br, addr, size);
+	if (region == NULL)
+		return (NULL);
+
+	if ((region->alloc_flags & BHNDB_ALLOC_FULFILL_ON_OVERCOMMIT) == 0)
+		return (NULL);
+
+	if ((dwa = bhndb_dw_steal(br, restore)) != NULL) {
+		*stolen = true;
+		return (dwa);
+	}
+
 	return (NULL);
 }
 
@@ -1649,9 +1696,8 @@ bhndb_io_resource_slow(struct bhndb_softc *sc, bus_addr_t addr,
  * Return a borrowed reference to a bridge resource allocation record capable
  * of handling bus I/O requests of @p size at @p addr.
  * 
- * This will either return a  reference to an existing allocation
- * record mapping the requested space, or will configure and return a free
- * allocation record.
+ * This will either return a reference to an existing allocation record mapping
+ * the requested space, or will configure and return a free allocation record.
  * 
  * Will panic if a usable record cannot be found.
  * 
@@ -1660,10 +1706,16 @@ bhndb_io_resource_slow(struct bhndb_softc *sc, bus_addr_t addr,
  * @param size The size of the I/O operation to be performed at @p addr. 
  * @param[out] offset The offset within the returned resource at which
  * to perform the I/O request.
+ * @param[out] stolen Set to true if the allocation record was stolen to fulfill
+ * this request. If a stolen allocation record is returned,
+ * bhndb_io_resource_restore() must be called upon completion of the bus I/O
+ * request.
+ * @param[out] restore If the allocation record was stolen, this will be set
+ * to the target that must be restored.
  */
 static inline struct bhndb_dw_alloc *
 bhndb_io_resource(struct bhndb_softc *sc, bus_addr_t addr, bus_size_t size,
-    bus_size_t *offset)
+    bus_size_t *offset, bool *stolen, bus_addr_t *restore)
 {
 	struct bhndb_resources	*br;
 	struct bhndb_dw_alloc	*dwa;
@@ -1691,7 +1743,8 @@ bhndb_io_resource(struct bhndb_softc *sc, bus_addr_t addr, bus_size_t size,
 	 * current operation.
 	 */
 	if (dwa == NULL) {
-		dwa = bhndb_io_resource_slow(sc, addr, size, offset);
+		dwa = bhndb_io_resource_slow(sc, addr, size, offset, stolen,
+		    restore);
 		if (dwa == NULL) {
 			panic("register windows exhausted attempting to map "
 			    "0x%llx-0x%llx\n", 
@@ -1720,6 +1773,7 @@ bhndb_io_resource(struct bhndb_softc *sc, bus_addr_t addr, bus_size_t size,
 
 	/* Calculate the offset and return */
 	*offset = (addr - dwa->target) + dwa->win->win_offset;
+	*stolen = false;
 	return (dwa);
 }
 
@@ -1733,12 +1787,14 @@ bhndb_io_resource(struct bhndb_softc *sc, bus_addr_t addr, bus_size_t size,
 	struct bhndb_dw_alloc	*dwa;				\
 	struct resource		*io_res;			\
 	bus_size_t		 io_offset;			\
+	bus_addr_t		 restore;		\
+	bool			 stolen;			\
 								\
 	sc = device_get_softc(dev);				\
 								\
 	BHNDB_LOCK(sc);						\
 	dwa = bhndb_io_resource(sc, rman_get_start(r->res) +	\
-	    offset, _io_size, &io_offset);			\
+	    offset, _io_size, &io_offset, &stolen, &restore);	\
 	io_res = dwa->parent_res;				\
 								\
 	KASSERT(!r->direct,					\
@@ -1748,6 +1804,10 @@ bhndb_io_resource(struct bhndb_softc *sc, bus_addr_t addr, bus_size_t size,
 	    ("i/o resource is not active"));
 
 #define	BHNDB_IO_COMMON_TEARDOWN()				\
+	if (stolen) {						\
+		bhndb_dw_return_stolen(sc->dev, sc->bus_res,	\
+		    dwa, restore);				\
+	}							\
 	BHNDB_UNLOCK(sc);
 
 /* Defines a bhndb_bus_read_* method implementation */

sys/dev/bhnd/bhndb/bhndb.h
@@ -90,6 +90,7 @@ struct bhndb_regwin {
 			bhnd_port_type	port_type;	/**< mapped port type */
 			u_int		port;		/**< mapped port number */
 			u_int		region;		/**< mapped region number */
+			bhnd_size_t	offset;		/**< mapped offset within the region */
 		} core;
 
 		/** SPROM register window (BHNDB_REGWIN_T_SPROM). */
@@ -150,6 +151,26 @@ typedef enum {
 } bhndb_priority_t;
 
 /**
+ * bhndb resource allocation flags.
+ */
+enum bhndb_alloc_flags {
+	/**
+	 * If resource overcommit prevents fulfilling a request for this
+	 * resource, an in-use resource should be be borrowed to fulfill the
+	 * request.
+	 * 
+	 * The only known use case is to support accessing the ChipCommon core
+	 * during Wi-Fi driver operation on early PCI Wi-Fi devices
+	 * (PCI_V0, SSB) that do not provide a dedicated ChipCommon register
+	 * window mapping; on such devices, device and firmware semantics
+	 * guarantee the safety of -- after disabling interrupts -- borrowing
+	 * the single dynamic register window that's been assigned to the D11
+	 * core to perform the few ChipCommon operations required by the driver.
+	 */
+	BHNDB_ALLOC_FULFILL_ON_OVERCOMMIT	= (1<<0),
+};
+
+/**
  * Port resource priority descriptor.
  */
 struct bhndb_port_priority {
@@ -157,6 +178,7 @@ struct bhndb_port_priority {
 	u_int			port;		/**< port */
 	u_int			region;		/**< region */
 	bhndb_priority_t	priority;	/**< port priority */
+	uint32_t		alloc_flags;	/**< port allocation flags (@see bhndb_alloc_flags) */
 };
 
 /**

sys/dev/bhnd/bhndb/bhndb_hwdata.c
@@ -76,19 +76,24 @@ __FBSDID("$FreeBSD$");
 	BHNDB_PORTS(__VA_ARGS__)		\
 }
 
-/* Define a port priority record for the type/port/region
- * triplet. */
-#define	BHNDB_PORT_PRIO(_type, _port, _region, _priority) {	\
+/* Define a port priority record for the type/port/region triplet, optionally
+ * specifying port allocation flags as the final argument */
+#define	BHNDB_PORT_PRIO(_type, _port, _region, _priority, ...)	\
+	_BHNDB_PORT_PRIO(_type, _port, _region, _priority, ## __VA_ARGS__, 0)
+
+#define	_BHNDB_PORT_PRIO(_type, _port, _region, _priority, _flags, ...)	\
+{								\
 	.type		= (BHND_PORT_ ## _type),		\
 	.port		= _port,				\
 	.region		= _region,				\
-	.priority	= (BHNDB_PRIORITY_ ## _priority)	\
+	.priority	= (BHNDB_PRIORITY_ ## _priority),	\
+	.alloc_flags	= (_flags)				\
 }
 
 /* Define a port priority record for the default (_type, 0, 0) type/port/region
  * triplet. */
-#define	BHNDB_PORT0_PRIO(_type, _priority)	\
-	BHNDB_PORT_PRIO(_type, 0, 0, _priority)
+#define	BHNDB_PORT0_PRIO(_type, _priority, ...)	\
+	BHNDB_PORT_PRIO(_type, 0, 0, _priority, ## __VA_ARGS__, 0)
 
 /**
  * Generic resource priority configuration usable with all currently supported
@@ -170,10 +175,14 @@ const struct bhndb_hw_priority bhndb_siba_priority_table[] = {
 	 * Agent ports are marked as 'NONE' on siba(4) devices, as they
 	 * will be fully mappable via register windows shared with the
 	 * device0.0 port.
+	 * 
+	 * To support early PCI_V0 devices, we enable FULFILL_ON_OVERCOMMIT for
+	 * ChipCommon.
 	 */
 	BHNDB_CLASS_PRIO(CC,		LOW,
 		/* Device Block */
-		BHNDB_PORT_PRIO(DEVICE,	0,	0,	LOW)
+		BHNDB_PORT_PRIO(DEVICE,	0,	0,	LOW,
+		    BHNDB_ALLOC_FULFILL_ON_OVERCOMMIT)
 	),
 
 	BHNDB_CLASS_PRIO(PMU,		LOW,

sys/dev/bhnd/bhndb/bhndb_pci.c
@@ -707,26 +707,28 @@ bhndb_pci_sprom_size(struct bhndb_pci_softc *sc)
  * Return the host resource providing a static mapping of the PCI core's
  * registers.
  * 
- * @param	sc	bhndb PCI driver state.
- * @param[out]	res	On success, the host resource containing our PCI
- *			core's register window.
- * @param[out]	offset	On success, the offset of the PCI core registers within
- * 			@p res.
+ * @param	sc		bhndb PCI driver state.
+ * @param	offset		The required readable offset within the PCI core
+ *				register block.
+ * @param	size		The required readable size at @p offset.
+ * @param[out]	res		On success, the host resource containing our PCI
+ *				core's register window.
+ * @param[out]	res_offset	On success, the @p offset relative to @p res.
  *
  * @retval 0		success
  * @retval ENXIO	if a valid static register window mapping the PCI core
  *			registers is not available.
  */
 static int
-bhndb_pci_get_core_regs(struct bhndb_pci_softc *sc, struct resource **res,
-    bus_size_t *offset)
+bhndb_pci_get_core_regs(struct bhndb_pci_softc *sc, bus_size_t offset,
+    bus_size_t size, struct resource **res, bus_size_t *res_offset)
 {
 	const struct bhndb_regwin	*win;
 	struct resource			*r;
 
-	/* Locate the static register window mapping the PCI core */
+	/* Locate the static register window mapping the requested offset */
 	win = bhndb_regwin_find_core(sc->bhndb.bus_res->cfg->register_windows,
-	    sc->pci_devclass, 0, BHND_PORT_DEVICE, 0, 0);
+	    sc->pci_devclass, 0, BHND_PORT_DEVICE, 0, 0, offset, size);
 	if (win == NULL) {
 		device_printf(sc->dev, "missing PCI core register window\n");
 		return (ENXIO);
@@ -739,8 +741,11 @@ bhndb_pci_get_core_regs(struct bhndb_pci_softc *sc, struct resource **res,
 		return (ENXIO);
 	}
 
+	KASSERT(offset >= win->d.core.offset, ("offset %#jx outside of "
+	    "register window", (uintmax_t)offset));
+
 	*res = r;
-	*offset = win->win_offset;
+	*res_offset = win->win_offset + (offset - win->d.core.offset);
 
 	return (0);
 }
@@ -761,18 +766,21 @@ bhndb_pci_write_core(struct bhndb_pci_softc *sc, bus_size_t offset,
 	bus_size_t	 r_offset;
 	int		 error;
 
-	if ((error = bhndb_pci_get_core_regs(sc, &r, &r_offset)))
-		panic("no PCI core registers: %d", error);
+	error = bhndb_pci_get_core_regs(sc, offset, width, &r, &r_offset);
+	if (error) {
+		panic("no PCI register window mapping %#jx+%#x: %d",
+		    (uintmax_t)offset, width, error);
+	}
 
 	switch (width) {
 	case 1:
-		bus_write_1(r, r_offset + offset, value);
+		bus_write_1(r, r_offset, value);
 		break;
 	case 2:
-		bus_write_2(r, r_offset + offset, value);
+		bus_write_2(r, r_offset, value);
 		break;
 	case 4:
-		bus_write_4(r, r_offset + offset, value);
+		bus_write_4(r, r_offset, value);
 		break;
 	default:
 		panic("invalid width: %u", width);
@@ -794,16 +802,19 @@ bhndb_pci_read_core(struct bhndb_pci_softc *sc, bus_size_t offset, u_int width)
 	bus_size_t	 r_offset;
 	int		 error;
 
-	if ((error = bhndb_pci_get_core_regs(sc, &r, &r_offset)))
-		panic("no PCI core registers: %d", error);
+	error = bhndb_pci_get_core_regs(sc, offset, width, &r, &r_offset);
+	if (error) {
+		panic("no PCI register window mapping %#jx+%#x: %d",
+		    (uintmax_t)offset, width, error);
+	}
 
 	switch (width) {
 	case 1:
-		return (bus_read_1(r, r_offset + offset));
+		return (bus_read_1(r, r_offset));
 	case 2:
-		return (bus_read_2(r, r_offset + offset));
+		return (bus_read_2(r, r_offset));
 	case 4:
-		return (bus_read_4(r, r_offset + offset));
+		return (bus_read_4(r, r_offset));
 	default:
 		panic("invalid width: %u", width);
 	}
@@ -1057,7 +1068,7 @@ bhndb_enable_pci_clocks(device_t dev)
 	pci_dev = device_get_parent(dev);
 
 	/* Only supported and required on PCI devices */
-	if (!bhndb_is_pcie_attached(dev))
+	if (bhndb_is_pcie_attached(dev))
 		return (0);
 
 	/* Read state of XTAL pin */

sys/dev/bhnd/bhndb/bhndb_pci_hwdata.c
@@ -312,7 +312,13 @@ static const struct bhndb_hwcfg bhndb_pci_hwcfg_v0 = {
 			.res		= { SYS_RES_MEMORY, PCIR_BAR(0) }
 		},
 		
-		/* bar0+0x1800: pci core registers */
+		/*
+		 * bar0+0x1800: pci core registers.
+		 * 
+		 * Does not include the SSB CFG registers found at the end of
+		 * the 4K core register block; these are mapped non-contigiously
+		 * by the next entry.
+		 */
 		{
 			.win_type	= BHNDB_REGWIN_T_CORE,
 			.win_offset	= BHNDB_PCI_V0_BAR0_PCIREG_OFFSET,
@@ -322,10 +328,27 @@ static const struct bhndb_hwcfg bhndb_pci_hwcfg_v0 = {
 				.unit	= 0,
 				.port	= 0,
 				.region	= 0,
+				.port_type = BHND_PORT_DEVICE,
+			},
+			.res		= { SYS_RES_MEMORY, PCIR_BAR(0) }
+		},
+
+		/* bar0+0x1E00: pci core (SSB CFG registers) */
+		{
+			.win_type	= BHNDB_REGWIN_T_CORE,
+			.win_offset	= BHNDB_PCI_V0_BAR0_PCISB_OFFSET	,
+			.win_size	= BHNDB_PCI_V0_BAR0_PCISB_SIZE,
+			.d.core = {
+				.class	= BHND_DEVCLASS_PCI,
+				.unit	= 0,
+				.port	= 0,
+				.region	= 0,
+				.offset	= BHNDB_PCI_V0_BAR0_PCISB_COREOFF,
 				.port_type = BHND_PORT_DEVICE
 			},
 			.res		= { SYS_RES_MEMORY, PCIR_BAR(0) }
 		},
+		
 		BHNDB_REGWIN_TABLE_END
 	},
 

sys/dev/bhnd/bhndb/bhndb_pcireg.h
@@ -40,7 +40,8 @@
  *	[offset+  size]	type	description
  * 	[0x0000+0x1000]	dynamic mapped backplane address space (window 0).
  * 	[0x1000+0x0800]	fixed	SPROM shadow
- * 	[0x1800+0x0800]	fixed	pci core registers
+ * 	[0x1800+0x0E00]	fixed	pci core device registers
+ *	[0x1E00+0x0200]	fixed	pci core siba config registers
  * 
  * == PCI_V1 ==
  * Applies to:
@@ -133,8 +134,11 @@
 #define	BHNDB_PCI_V0_BAR0_WIN0_SIZE	0x1000
 #define	BHNDB_PCI_V0_BAR0_SPROM_OFFSET	0x1000	/* bar0 + 4K accesses sprom shadow (in pci core) */
 #define BHNDB_PCI_V0_BAR0_SPROM_SIZE	0x0800
-#define	BHNDB_PCI_V0_BAR0_PCIREG_OFFSET	0x1800	/* bar0 + 6K accesses pci core registers */
-#define	BHNDB_PCI_V0_BAR0_PCIREG_SIZE	0x0800
+#define	BHNDB_PCI_V0_BAR0_PCIREG_OFFSET	0x1800	/* bar0 + 6K accesses pci core registers (not including SSB CFG registers) */
+#define	BHNDB_PCI_V0_BAR0_PCIREG_SIZE	0x0E00
+#define	BHNDB_PCI_V0_BAR0_PCISB_OFFSET	0x1E00	/* bar0 + 7.5K accesses pci core's SSB CFG register blocks */
+#define	BHNDB_PCI_V0_BAR0_PCISB_SIZE	0x0200
+#define	BHNDB_PCI_V0_BAR0_PCISB_COREOFF	0xE00	/* mapped offset relative to the core base address */
 
 /* PCI_V1 */
 #define	BHNDB_PCI_V1_BAR0_WIN0_CONTROL	0x80	/* backplane address space accessed by BAR0/WIN0 */

sys/dev/bhnd/bhndb/bhndb_private.h
@@ -70,6 +70,7 @@ int				 bhndb_add_resource_region(
 				     struct bhndb_resources *br,
 				     bhnd_addr_t addr, bhnd_size_t size,
 				     bhndb_priority_t priority,
+				     uint32_t alloc_flags,
 				     const struct bhndb_regwin *static_regwin);
 
 int				 bhndb_find_resource_limits(
@@ -92,6 +93,10 @@ void				 bhndb_deregister_intr_handler(
 struct bhndb_intr_handler	*bhndb_find_intr_handler(
 				     struct bhndb_resources *br,
 				     void *cookiep);
+
+bool				 bhndb_has_static_region_mapping(
+				     struct bhndb_resources *br,
+				     bhnd_addr_t addr, bhnd_size_t size);
 
 struct bhndb_region		*bhndb_find_resource_region(
 				     struct bhndb_resources *br,
@@ -120,9 +125,23 @@ int				 bhndb_dw_set_addr(device_t dev,
 				     struct bhndb_dw_alloc *dwa,
 				     bus_addr_t addr, bus_size_t size);
 
+struct bhndb_dw_alloc		*bhndb_dw_steal(struct bhndb_resources *br,
+				     bus_addr_t *saved);
+
+void				 bhndb_dw_return_stolen(device_t dev,
+				     struct bhndb_resources *br,
+				     struct bhndb_dw_alloc *dwa,
+				     bus_addr_t saved);
+
 const struct bhndb_hw_priority	*bhndb_hw_priority_find_core(
 				     const struct bhndb_hw_priority *table,
 				     struct bhnd_core_info *core);
+
+const struct bhndb_port_priority *bhndb_hw_priorty_find_port(
+				     const struct bhndb_hw_priority *table,
+				     struct bhnd_core_info *core,
+				     bhnd_port_type port_type, u_int port,
+				     u_int region);
 
 
 /**
@@ -152,6 +171,7 @@ struct bhndb_region {
 	bhnd_addr_t			 addr;		/**< start of mapped range */
 	bhnd_size_t			 size;		/**< size of mapped range */
 	bhndb_priority_t		 priority;	/**< direct resource allocation priority */
+	uint32_t			 alloc_flags;	/**< resource allocation flags (@see bhndb_alloc_flags) */
 	const struct bhndb_regwin	*static_regwin;	/**< fixed mapping regwin, if any */
 
 	STAILQ_ENTRY(bhndb_region)	 link;
@@ -185,6 +205,7 @@ struct bhndb_resources {
 
 	STAILQ_HEAD(, bhndb_region) 	 bus_regions;	/**< bus region descriptors */
 
+	struct mtx			 dw_steal_mtx;	/**< spinlock must be held when stealing a dynamic window allocation */
 	struct bhndb_dw_alloc		*dw_alloc;	/**< dynamic window allocation records */
 	size_t				 dwa_count;	/**< number of dynamic windows available. */
 	bitstr_t			*dwa_freelist;	/**< dynamic window free list */

commit 2f7c289ef0c9b91d7e55c57fd7d716becf0573e6

bhndb(4): Fix two register window overcommit bugs introduced in r326297:
- The window target must always be updated when stealing a register window.
- Fix missing initialization of bhndb(4) region alloc_flags when
  registering statically mapped port regions (caught by scan-build).

Approved by:	adrian (mentor, implicit)
Sponsored by:	The FreeBSD Foundation

sys/dev/bhnd/bhndb/bhndb.c
@@ -340,7 +340,7 @@ bhndb_init_region_cfg(struct bhndb_softc *sc, bhnd_erom_t *erom,
 			 * always HIGH.
 			 */
 			error = bhndb_add_resource_region(br, addr, size,
-			    BHNDB_PRIORITY_HIGH, 0, regw);
+			    BHNDB_PRIORITY_HIGH, alloc_flags, regw);
 			if (error)
 				return (error);
 		}
@@ -1634,14 +1634,33 @@ bhndb_deactivate_bhnd_resource(device_t dev, device_t child,
 }
 
 /**
- * Slow path for bhndb_io_resource().
- *
- * Iterates over the existing allocated dynamic windows looking for a viable
- * in-use region; the first matching region is returned.
+ * Find the best available bridge resource allocation record capable of handling
+ * bus I/O requests of @p size at @p addr.
+ * 
+ * In order of preference, this function will either:
+ * 
+ * - Configure and return a free allocation record
+ * - Return an existing allocation record mapping the requested space, or
+ * - Steal, configure, and return an in-use allocation record.
+ * 
+ * Will panic if a usable record cannot be found.
+ * 
+ * @param sc Bridge driver state.
+ * @param addr The I/O target address.
+ * @param size The size of the I/O operation to be performed at @p addr. 
+ * @param[out] borrowed Set to true if the allocation record was borrowed to
+ * fulfill this request; the borrowed record maps the target address range,
+ * and must not be modified.
+ * @param[out] stolen Set to true if the allocation record was stolen to fulfill
+ * this request. If a stolen allocation record is returned,
+ * bhndb_io_resource_restore() must be called upon completion of the bus I/O
+ * request.
+ * @param[out] restore If the allocation record was stolen, this will be set
+ * to the target that must be restored.
  */
 static struct bhndb_dw_alloc *
-bhndb_io_resource_slow(struct bhndb_softc *sc, bus_addr_t addr, bus_size_t size,
-    bus_size_t *offset, bool *stolen, bus_addr_t *restore)
+bhndb_io_resource_get_window(struct bhndb_softc *sc, bus_addr_t addr,
+    bus_size_t size, bool *borrowed, bool *stolen, bus_addr_t *restore)
 {
 	struct bhndb_resources	*br;
 	struct bhndb_dw_alloc	*dwa;
@@ -1650,6 +1669,12 @@ bhndb_io_resource_slow(struct bhndb_softc *sc, bus_addr_t addr, bus_size_t size,
 	BHNDB_LOCK_ASSERT(sc, MA_OWNED);
 
 	br = sc->bus_res;
+	*borrowed = false;
+	*stolen = false;
+
+	/* Try to fetch a free window */
+	if ((dwa = bhndb_dw_next_free(br)) != NULL)
+		return (dwa);
 
 	/* Search for an existing dynamic mapping of this address range.
 	 * Static regions are not searched, as a statically mapped
@@ -1671,16 +1696,12 @@ bhndb_io_resource_slow(struct bhndb_softc *sc, bus_addr_t addr, bus_size_t size,
 			continue;
 
 		/* Found */
-		*offset = dwa->win->win_offset;
-		*offset += addr - dwa->target;
-
-		*stolen = false;
+		*borrowed = true;
 		return (dwa);
 	}
 
-	/* No existing dynamic mapping found. We'll need to check for a defined
-	 * region to determine whether we can fulfill this request by
-	 * stealing from an existing allocated register window */
+	/* Try to steal a window; this should only be required on very early
+	 * PCI_V0 (BCM4318, etc) Wi-Fi chipsets */
 	region = bhndb_find_resource_region(br, addr, size);
 	if (region == NULL)
 		return (NULL);
@@ -1688,12 +1709,16 @@ bhndb_io_resource_slow(struct bhndb_softc *sc, bus_addr_t addr, bus_size_t size,
 	if ((region->alloc_flags & BHNDB_ALLOC_FULFILL_ON_OVERCOMMIT) == 0)
 		return (NULL);
 
+	/* Steal a window. This acquires our backing spinlock, disabling
+	 * interrupts; the spinlock will be released by
+	 * bhndb_dw_return_stolen() */
 	if ((dwa = bhndb_dw_steal(br, restore)) != NULL) {
 		*stolen = true;
 		return (dwa);
 	}
 
-	return (NULL);
+	panic("register windows exhausted attempting to map 0x%llx-0x%llx\n", 
+	    (unsigned long long) addr, (unsigned long long) addr+size-1);
 }
 
 /**
@@ -1721,43 +1746,14 @@ static inline struct bhndb_dw_alloc *
 bhndb_io_resource(struct bhndb_softc *sc, bus_addr_t addr, bus_size_t size,
     bus_size_t *offset, bool *stolen, bus_addr_t *restore)
 {
-	struct bhndb_resources	*br;
 	struct bhndb_dw_alloc	*dwa;
+	bool			 borrowed;
 	int			 error;
 
 	BHNDB_LOCK_ASSERT(sc, MA_OWNED);
 
-	br = sc->bus_res;
-
-	/* Try to fetch a free window */
-	dwa = bhndb_dw_next_free(br);
-
-	/*
-	 * If no dynamic windows are available, look for an existing
-	 * region that maps the target range. 
-	 * 
-	 * If none are found, this is a child driver bug -- our window
-	 * over-commit should only fail in the case where a child driver leaks
-	 * resources, or perform operations out-of-order.
-	 * 
-	 * Broadcom HND chipsets are designed to not require register window
-	 * swapping during execution; as long as the child devices are
-	 * attached/detached correctly, using the hardware's required order
-	 * of operations, there should always be a window available for the
-	 * current operation.
-	 */
-	if (dwa == NULL) {
-		dwa = bhndb_io_resource_slow(sc, addr, size, offset, stolen,
-		    restore);
-		if (dwa == NULL) {
-			panic("register windows exhausted attempting to map "
-			    "0x%llx-0x%llx\n", 
-			    (unsigned long long) addr,
-			    (unsigned long long) addr+size-1);
-		}
-
-		return (dwa);
-	}
+	dwa = bhndb_io_resource_get_window(sc, addr, size, &borrowed, stolen,
+	    restore);
 
 	/* Adjust the window if the I/O request won't fit in the current
 	 * target range. */
@@ -1765,6 +1761,14 @@ bhndb_io_resource(struct bhndb_softc *sc, bus_addr_t addr, bus_size_t size,
 	    addr > dwa->target + dwa->win->win_size ||
 	    (dwa->target + dwa->win->win_size) - addr < size)
 	{
+		/* Cannot modify target of borrowed windows */
+		if (borrowed) {
+			panic("borrowed register window does not map expected "
+			    "range 0x%llx-0x%llx\n", 
+			    (unsigned long long) addr,
+			    (unsigned long long) addr+size-1);
+		}
+	
 		error = bhndb_dw_set_addr(sc->dev, sc->bus_res, dwa, addr,
 		    size);
 		if (error) {
@@ -1777,7 +1781,6 @@ bhndb_io_resource(struct bhndb_softc *sc, bus_addr_t addr, bus_size_t size,
 
 	/* Calculate the offset and return */
 	*offset = (addr - dwa->target) + dwa->win->win_offset;
-	*stolen = false;
 	return (dwa);
 }
 