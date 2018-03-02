markjdb/freebsd

commit 74257289b4314d23596f4dc454b147422dcd131f

Refactoring of reading and writing of the UFS/FFS superblock. Specifically reading is done if ffs_sbget() and writing is done in ffs_sbput(). These functions are exported to libufs via the sbget() and sbput() functions which then used in the various filesystem utilities. This work is in preparation for adding subperblock check hashes.
No functional change intended.

Reviewed by: kib


git-svn-id: https://svn.freebsd.org/base/head@328426 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

lib/libufs/sblock.c
@@ -47,79 +47,48 @@ __FBSDID("$FreeBSD$");
 
 #include <libufs.h>
 
-static int superblocks[] = SBLOCKSEARCH;
-
 int
 sbread(struct uufsd *disk)
 {
-	uint8_t block[MAXBSIZE];
 	struct fs *fs;
-	int sb, superblock;
-	int i, size, blks;
-	uint8_t *space;
 
 	ERROR(disk, NULL);
 
-	fs = &disk->d_fs;
-	superblock = superblocks[0];
-
-	for (sb = 0; (superblock = superblocks[sb]) != -1; sb++) {
-		if (bread(disk, superblock, disk->d_sb, SBLOCKSIZE) == -1) {
+	if ((errno = sbget(disk->d_fd, &fs, -1)) != 0) {
+		switch (errno) {
+		case EIO:
 			ERROR(disk, "non-existent or truncated superblock");
-			return (-1);
-		}
-		if (fs->fs_magic == FS_UFS1_MAGIC)
-			disk->d_ufs = 1;
-		if (fs->fs_magic == FS_UFS2_MAGIC &&
-		    fs->fs_sblockloc == superblock)
-			disk->d_ufs = 2;
-		if (fs->fs_bsize <= MAXBSIZE &&
-		    (size_t)fs->fs_bsize >= sizeof(*fs)) {
-			if (disk->d_ufs)
-				break;
+			break;
+		case ENOENT:
+			ERROR(disk, "no usable known superblock found");
+			break;
+		case ENOSPC:
+			ERROR(disk, "failed to allocate space for superblock "
+			    "information");
+			break;
+		case EINVAL:
+			ERROR(disk, "The previous newfs operation on this "
+			    "volume did not complete.\nYou must complete "
+			    "newfs before using this volume.");
+			break;
+		default:
+			ERROR(disk, "unknown superblock read error");
+			errno = EIO;
+			break;
 		}
 		disk->d_ufs = 0;
-	}
-	if (superblock == -1 || disk->d_ufs == 0) {
-		/*
-		 * Other error cases will result in errno being set, here we
-		 * must set it to indicate no superblock could be found with
-		 * which to associate this disk/filesystem.
-		 */
-		ERROR(disk, "no usable known superblock found");
-		errno = ENOENT;
 		return (-1);
 	}
+	memcpy(&disk->d_fs, fs, fs->fs_sbsize);
+	free(fs);
+	fs = &disk->d_fs;
+	if (fs->fs_magic == FS_UFS1_MAGIC)
+		disk->d_ufs = 1;
+	if (fs->fs_magic == FS_UFS2_MAGIC)
+		disk->d_ufs = 2;
 	disk->d_bsize = fs->fs_fsize / fsbtodb(fs, 1);
-	disk->d_sblock = superblock / disk->d_bsize;
-	/*
-	 * Read in the superblock summary information.
-	 */
-	size = fs->fs_cssize;
-	blks = howmany(size, fs->fs_fsize);
-	size += fs->fs_ncg * sizeof(int32_t);
-	space = malloc(size);
-	if (space == NULL) {
-		ERROR(disk, "failed to allocate space for summary information");
-		return (-1);
-	}
-	fs->fs_csp = (struct csum *)space;
-	for (i = 0; i < blks; i += fs->fs_frag) {
-		size = fs->fs_bsize;
-		if (i + fs->fs_frag > blks)
-			size = (blks - i) * fs->fs_fsize;
-		if (bread(disk, fsbtodb(fs, fs->fs_csaddr + i), block, size)
-		    == -1) {
-			ERROR(disk, "Failed to read sb summary information");
-			free(fs->fs_csp);
-			return (-1);
-		}
-		bcopy(block, space, size);
-		space += size;
-	}
-	fs->fs_maxcluster = (uint32_t *)space;
+	disk->d_sblock = fs->fs_sblockloc / disk->d_bsize;
 	disk->d_sbcsum = fs->fs_csp;
-
 	return (0);
 }
 
@@ -127,45 +96,107 @@ int
 sbwrite(struct uufsd *disk, int all)
 {
 	struct fs *fs;
-	int blks, size;
-	uint8_t *space;
-	unsigned i;
 
 	ERROR(disk, NULL);
 
 	fs = &disk->d_fs;
-
-	if (!disk->d_sblock) {
-		disk->d_sblock = disk->d_fs.fs_sblockloc / disk->d_bsize;
-	}
-
-	if (bwrite(disk, disk->d_sblock, fs, SBLOCKSIZE) == -1) {
-		ERROR(disk, "failed to write superblock");
+	if ((errno = sbput(disk->d_fd, fs, all ? fs->fs_ncg : 0)) != 0) {
+		switch (errno) {
+		case EIO:
+			ERROR(disk, "failed to write superblock");
+			break;
+		default:
+			ERROR(disk, "unknown superblock write error");
+			errno = EIO;
+			break;
+		}
 		return (-1);
 	}
-	/*
-	 * Write superblock summary information.
-	 */
-	blks = howmany(fs->fs_cssize, fs->fs_fsize);
-	space = (uint8_t *)disk->d_sbcsum;
-	for (i = 0; i < blks; i += fs->fs_frag) {
-		size = fs->fs_bsize;
-		if (i + fs->fs_frag > blks)
-			size = (blks - i) * fs->fs_fsize;
-		if (bwrite(disk, fsbtodb(fs, fs->fs_csaddr + i), space, size)
-		    == -1) {
-			ERROR(disk, "Failed to write sb summary information");
+	return (0);
+}
+
+/*
+ * These are the low-level functions that actually read and write
+ * the superblock and its associated data. The actual work is done by
+ * the functions ffs_sbget and ffs_sbput in /sys/ufs/ffs/ffs_subr.c.
+ */
+static int use_pread(void *devfd, off_t loc, void **bufp, int size);
+static int use_pwrite(void *devfd, off_t loc, void *buf, int size);
+
+/*
+ * Read a superblock from the devfd device allocating memory returned
+ * in fsp. Also read the superblock summary information.
+ */
+int
+sbget(int devfd, struct fs **fsp, off_t sblockloc)
+{
+
+	return (ffs_sbget(&devfd, fsp, sblockloc, "user", use_pread));
+}
+
+/*
+ * A read function for use by user-level programs using libufs.
+ */
+static int
+use_pread(void *devfd, off_t loc, void **bufp, int size)
+{
+	int fd;
+
+	fd = *(int *)devfd;
+	if ((*bufp = malloc(size)) == NULL)
+		return (ENOSPC);
+	if (pread(fd, *bufp, size, loc) != size)
+		return (EIO);
+	return (0);
+}
+
+/*
+ * Write a superblock to the devfd device from the memory pointed to by fs.
+ * Also write out the superblock summary information but do not free the
+ * summary information memory.
+ *
+ * Additionally write out numaltwrite of the alternate superblocks. Use
+ * fs->fs_ncg to write out all of the alternate superblocks.
+ */
+int
+sbput(int devfd, struct fs *fs, int numaltwrite)
+{
+	struct csum *savedcsp;
+	off_t savedactualloc;
+	int i, error;
+
+	if ((error = ffs_sbput(&devfd, fs, fs->fs_sblockactualloc,
+	     use_pwrite)) != 0)
+		return (error);
+	if (numaltwrite == 0)
+		return (0);
+	savedactualloc = fs->fs_sblockactualloc;
+	savedcsp = fs->fs_csp;
+	fs->fs_csp = NULL;
+	for (i = 0; i < numaltwrite; i++) {
+		fs->fs_sblockactualloc = dbtob(fsbtodb(fs, cgsblock(fs, i)));
+		if ((error = ffs_sbput(&devfd, fs, fs->fs_sblockactualloc,
+		     use_pwrite)) != 0) {
+			fs->fs_sblockactualloc = savedactualloc;
+			fs->fs_csp = savedcsp;
 			return (-1);
 		}
-		space += size;
 	}
-	if (all) {
-		for (i = 0; i < fs->fs_ncg; i++)
-			if (bwrite(disk, fsbtodb(fs, cgsblock(fs, i)),
-			    fs, SBLOCKSIZE) == -1) {
-				ERROR(disk, "failed to update a superblock");
-				return (-1);
-			}
-	}
+	fs->fs_sblockactualloc = savedactualloc;
+	fs->fs_csp = savedcsp;
+	return (0);
+}
+
+/*
+ * A write function for use by user-level programs using sbput in libufs.
+ */
+static int
+use_pwrite(void *devfd, off_t loc, void *buf, int size)
+{
+	int fd;
+
+	fd = *(int *)devfd;
+	if (pwrite(fd, buf, size, loc) != size)
+		return (EIO);
 	return (0);
 }

stand/libsa/ufs.c
@@ -133,6 +133,11 @@ static int	block_map(struct open_file *, ufs2_daddr_t, ufs2_daddr_t *);
 static int	buf_read_file(struct open_file *, char **, size_t *);
 static int	buf_write_file(struct open_file *, char *, size_t *);
 static int	search_directory(char *, struct open_file *, ino_t *);
+static int	ufs_use_sa_read(void *, off_t, void **, int);
+
+/* from ffs_subr.c */
+int	ffs_sbget(void *, struct fs **, off_t, char *,
+	    int (*)(void *, off_t, void **, int));
 
 /*
  * Read a new inode into a file structure.
@@ -485,8 +490,6 @@ search_directory(name, f, inumber_p)
 	return (ENOENT);
 }
 
-static int sblock_try[] = SBLOCKSEARCH;
-
 /*
  * Open a file.
  */
@@ -512,31 +515,11 @@ ufs_open(upath, f)
 	bzero(fp, sizeof(struct file));
 	f->f_fsdata = (void *)fp;
 
-	/* allocate space and read super block */
-	fs = malloc(SBLOCKSIZE);
-	fp->f_fs = fs;
+	/* read super block */
 	twiddle(1);
-	/*
-	 * Try reading the superblock in each of its possible locations.
-	 */
-	for (i = 0; sblock_try[i] != -1; i++) {
-		rc = (f->f_dev->dv_strategy)(f->f_devdata, F_READ,
-		    sblock_try[i] / DEV_BSIZE, SBLOCKSIZE,
-		    (char *)fs, &buf_size);
-		if (rc)
-			goto out;
-		if ((fs->fs_magic == FS_UFS1_MAGIC ||
-		     (fs->fs_magic == FS_UFS2_MAGIC &&
-		      fs->fs_sblockloc == sblock_try[i])) &&
-		    buf_size == SBLOCKSIZE &&
-		    fs->fs_bsize <= MAXBSIZE &&
-		    fs->fs_bsize >= sizeof(struct fs))
-			break;
-	}
-	if (sblock_try[i] == -1) {
-		rc = EINVAL;
+	if ((rc = ffs_sbget(f, &fs, -1, 0, ufs_use_sa_read)) != 0)
 		goto out;
-	}
+	fp->f_fs = fs;
 	/*
 	 * Calculate indirect block levels.
 	 */
@@ -691,6 +674,28 @@ out:
 		free(fp);
 	}
 	return (rc);
+}
+
+/*
+ * A read function for use by standalone-layer routines.
+ */
+static int
+ufs_use_sa_read(void *devfd, off_t loc, void **bufp, int size)
+{
+	struct open_file *f;
+	size_t buf_size;
+	int error;
+
+	f = (struct open_file *)devfd;
+	if ((*bufp = malloc(size)) == NULL)
+		return (ENOSPC);
+	error = (f->f_dev->dv_strategy)(f->f_devdata, F_READ, loc / DEV_BSIZE,
+	    size, *bufp, &buf_size);
+	if (error != 0)
+		return (error);
+	if (buf_size != size)
+		return (EIO);
+	return (0);
 }
 
 static int

sys/geom/geom_io.c
@@ -949,6 +949,29 @@ g_read_data(struct g_consumer *cp, off_t offset, off_t length, int *error)
 	return (ptr);
 }
 
+/*
+ * A read function for use by ffs_sbget when used by GEOM-layer routines.
+ */
+int
+g_use_g_read_data(void *devfd, off_t loc, void **bufp, int size)
+{
+	struct g_consumer *cp;
+
+	cp = (struct g_consumer *)devfd;
+	/*
+	 * Take care not to issue an invalid I/O request. The offset of
+	 * the superblock candidate must be multiples of the provider's
+	 * sector size, otherwise an FFS can't exist on the provider
+	 * anyway.
+	 */
+	if (loc % cp->provider->sectorsize != 0)
+		return (ENOENT);
+	*bufp = g_read_data(cp, loc, size, NULL);
+	if (*bufp == NULL)
+		return (ENOENT);
+	return (0);
+}
+
 int
 g_write_data(struct g_consumer *cp, off_t offset, void *ptr, off_t length)
 {
@@ -969,6 +992,16 @@ g_write_data(struct g_consumer *cp, off_t offset, void *ptr, off_t length)
 	error = biowait(bp, "gwrite");
 	g_destroy_bio(bp);
 	return (error);
+}
+
+/*
+ * A write function for use by ffs_sbput when used by GEOM-layer routines.
+ */
+int
+g_use_g_write_data(void *devfd, off_t loc, void *buf, int size)
+{
+
+	return (g_write_data((struct g_consumer *)devfd, loc, buf, size));
 }
 
 int

sys/geom/journal/g_journal_ufs.c
@@ -46,8 +46,6 @@ __FBSDID("$FreeBSD$");
 #include <geom/geom.h>
 #include <geom/journal/g_journal.h>
 
-static const int superblocks[] = SBLOCKSEARCH;
-
 static int
 g_journal_ufs_clean(struct mount *mp)
 {
@@ -70,33 +68,25 @@ static void
 g_journal_ufs_dirty(struct g_consumer *cp)
 {
 	struct fs *fs;
-	int error, i, sb;
+	int error;
 
-	if (SBLOCKSIZE % cp->provider->sectorsize != 0)
+	if (SBLOCKSIZE % cp->provider->sectorsize != 0 ||
+	    ffs_sbget(cp, &fs, -1, NULL, g_use_g_read_data) != 0) {
+		GJ_DEBUG(0, "Cannot find superblock to mark file system %s "
+		    "as dirty.", cp->provider->name);
 		return;
-	for (i = 0; (sb = superblocks[i]) != -1; i++) {
-		if (sb % cp->provider->sectorsize != 0)
-			continue;
-		fs = g_read_data(cp, sb, SBLOCKSIZE, NULL);
-		if (fs == NULL)
-			continue;
-		if (fs->fs_magic != FS_UFS1_MAGIC &&
-		    fs->fs_magic != FS_UFS2_MAGIC) {
-			g_free(fs);
-			continue;
-		}
-		GJ_DEBUG(0, "clean=%d flags=0x%x", fs->fs_clean, fs->fs_flags);
-		fs->fs_clean = 0;
-		fs->fs_flags |= FS_NEEDSFSCK | FS_UNCLEAN;
-		error = g_write_data(cp, sb, fs, SBLOCKSIZE);
-		g_free(fs);
-		if (error != 0) {
-			GJ_DEBUG(0, "Cannot mark file system %s as dirty "
-			    "(error=%d).", cp->provider->name, error);
-		} else {
-			GJ_DEBUG(0, "File system %s marked as dirty.",
-			    cp->provider->name);
-		}
+	}
+	GJ_DEBUG(0, "clean=%d flags=0x%x", fs->fs_clean, fs->fs_flags);
+	fs->fs_clean = 0;
+	fs->fs_flags |= FS_NEEDSFSCK | FS_UNCLEAN;
+	error = ffs_sbput(cp, fs, fs->fs_sblockloc, g_use_g_write_data);
+	g_free(fs);
+	if (error != 0) {
+		GJ_DEBUG(0, "Cannot mark file system %s as dirty "
+		    "(error=%d).", cp->provider->name, error);
+	} else {
+		GJ_DEBUG(0, "File system %s marked as dirty.",
+		    cp->provider->name);
 	}
 }
 

sys/geom/label/g_label_ufs.c
@@ -32,11 +32,14 @@ __FBSDID("$FreeBSD$");
 
 #include <sys/param.h>
 #include <sys/systm.h>
-#include <sys/kernel.h>
 #include <sys/malloc.h>
+#include <sys/vnode.h>
 
 #include <ufs/ufs/dinode.h>
 #include <ufs/ffs/fs.h>
+#include <ufs/ufs/quota.h>
+#include <ufs/ufs/extattr.h>
+#include <ufs/ffs/ffs_extern.h>
 
 #include <geom/geom.h>
 #include <geom/label/g_label.h>
@@ -56,106 +59,63 @@ __FBSDID("$FreeBSD$");
 				< G_LABEL_UFS_MAXDIFF )
 #define	G_LABEL_UFS_MAXDIFF	0x100
 
-static const int superblocks[] = SBLOCKSEARCH;
-
+/*
+ * Try to find a superblock on the provider. If successful, then
+ * check that the size in the superblock corresponds to the size
+ * of the underlying provider. Finally, look for a volume label
+ * and create an appropriate provider based on that.
+ */
 static void
 g_label_ufs_taste_common(struct g_consumer *cp, char *label, size_t size, int what)
 {
 	struct g_provider *pp;
-	int sb, superblock;
 	struct fs *fs;
 
 	g_topology_assert_not();
 	pp = cp->provider;
 	label[0] = '\0';
 
-	if (SBLOCKSIZE % cp->provider->sectorsize != 0)
+	if (SBLOCKSIZE % pp->sectorsize != 0 ||
+	    ffs_sbget(cp, &fs, -1, NULL, g_use_g_read_data) != 0)
 		return;
-
 	/*
-	 * Walk through the standard places that superblocks hide and look
-	 * for UFS magic. If we find magic, then check that the size in the
-	 * superblock corresponds to the size of the underlying provider.
-	 * Finally, look for a volume label and create an appropriate
-	 * provider based on that.
+	 * Check for magic. We also need to check if file system size
+	 * is almost equal to providers size, because sysinstall(8)
+	 * used to bogusly put first partition at offset 0
+	 * instead of 16, and glabel/ufs would find file system on slice
+	 * instead of partition.
+	 *
+	 * In addition, media size can be a bit bigger than file system
+	 * size. For instance, mkuzip can append bytes to align data
+	 * to large sector size (it improves compression rates).
 	 */
-	for (sb = 0; (superblock = superblocks[sb]) != -1; sb++) {
-		/*
-		 * Take care not to issue an invalid I/O request. The offset of
-		 * the superblock candidate must be multiples of the provider's
-		 * sector size, otherwise an FFS can't exist on the provider
-		 * anyway.
-		 */
-		if (superblock % cp->provider->sectorsize != 0)
-			continue;
-
-		fs = (struct fs *)g_read_data(cp, superblock, SBLOCKSIZE, NULL);
-		if (fs == NULL)
-			continue;
-		/*
-		 * Check for magic. We also need to check if file system size
-		 * is almost equal to providers size, because sysinstall(8)
-		 * used to bogusly put first partition at offset 0
-		 * instead of 16, and glabel/ufs would find file system on slice
-		 * instead of partition.
-		 *
-		 * In addition, media size can be a bit bigger than file system
-		 * size. For instance, mkuzip can append bytes to align data
-		 * to large sector size (it improves compression rates).
-		 */
-		switch (fs->fs_magic){
-		case FS_UFS1_MAGIC:
-		case FS_UFS2_MAGIC:
-			G_LABEL_DEBUG(1, "%s %s params: %jd, %d, %d, %jd\n",
-				fs->fs_magic == FS_UFS1_MAGIC ? "UFS1" : "UFS2",
-				pp->name, pp->mediasize, fs->fs_fsize,
-				fs->fs_old_size, fs->fs_providersize);
-			break;
-		default:
-			break;
-		}
-
-		if (fs->fs_magic == FS_UFS1_MAGIC && fs->fs_fsize > 0 &&
-		    ( G_LABEL_UFS_CMP(pp, fs, fs_old_size)
-			|| G_LABEL_UFS_CMP(pp, fs, fs_providersize))) {
-		    	/* Valid UFS1. */
-		} else if (fs->fs_magic == FS_UFS2_MAGIC && fs->fs_fsize > 0 &&
-		    ( G_LABEL_UFS_CMP(pp, fs, fs_size)
-			|| G_LABEL_UFS_CMP(pp, fs, fs_providersize))) {
-		    	/* Valid UFS2. */
-		} else {
-			g_free(fs);
-			continue;
-		}
-		if (fs->fs_sblockloc != superblock || fs->fs_ncg < 1 ||
-		    fs->fs_bsize < MINBSIZE ||
-		    fs->fs_bsize < sizeof(struct fs)) {
-			g_free(fs);
-			continue;
-		}
-		G_LABEL_DEBUG(1, "%s file system detected on %s.",
-		    fs->fs_magic == FS_UFS1_MAGIC ? "UFS1" : "UFS2", pp->name);
-		switch (what) {
-		case G_LABEL_UFS_VOLUME:
-			/* Check for volume label */
-			if (fs->fs_volname[0] == '\0') {
-				g_free(fs);
-				continue;
-			}
+	if (fs->fs_magic == FS_UFS1_MAGIC && fs->fs_fsize > 0 &&
+	    ( G_LABEL_UFS_CMP(pp, fs, fs_old_size)
+		|| G_LABEL_UFS_CMP(pp, fs, fs_providersize))) {
+		/* Valid UFS1. */
+	} else if (fs->fs_magic == FS_UFS2_MAGIC && fs->fs_fsize > 0 &&
+	    ( G_LABEL_UFS_CMP(pp, fs, fs_size)
+		|| G_LABEL_UFS_CMP(pp, fs, fs_providersize))) {
+		/* Valid UFS2. */
+	} else {
+		g_free(fs);
+		return;
+	}
+	G_LABEL_DEBUG(1, "%s file system detected on %s.",
+	    fs->fs_magic == FS_UFS1_MAGIC ? "UFS1" : "UFS2", pp->name);
+	switch (what) {
+	case G_LABEL_UFS_VOLUME:
+		/* Check for volume label */
+		if (fs->fs_volname[0] != '\0')
 			strlcpy(label, fs->fs_volname, size);
-			break;
-		case G_LABEL_UFS_ID:
-			if (fs->fs_id[0] == 0 && fs->fs_id[1] == 0) {
-				g_free(fs);
-				continue;
-			}
+		break;
+	case G_LABEL_UFS_ID:
+		if (fs->fs_id[0] != 0 || fs->fs_id[1] != 0)
 			snprintf(label, size, "%08x%08x", fs->fs_id[0],
 			    fs->fs_id[1]);
-			break;
-		}
-		g_free(fs);
 		break;
 	}
+	g_free(fs);
 }
 
 static void

commit 0270e4bb44314fe753321e37ba0989a535142f95

This change is some refactoring of Mark Johnston's changes in r329375 to fix the memory leak that I introduced in r328426. Instead of trying to clear up the possible memory leak in all the clients, I ensure that it gets cleaned up in the source (e.g., ffs_sbget ensures that memory is always freed if it returns an error).
The original change in r328426 was a bit sparse in its description.
So I am expanding on its description here (thanks cem@ and rgrimes@
for your encouragement for my longer commit messages).

In preparation for adding check hashing to superblocks, r328426 is
a refactoring of the code to get the reading/writing of the superblock
into one place. Unlike the cylinder group reading/writing which
ends up in two places (ffs_getcg/ffs_geom_strategy in the kernel
and cgget/cgput in libufs), I have the core superblock functions
just in the kernel (ffs_sbfetch/ffs_sbput in ffs_subr.c which is
already imported into utilities like fsck_ffs as well as libufs to
implement sbget/sbput). The ffs_sbfetch and ffs_sbput functions
take a function pointer to do the actual I/O for which there are
four variants:

    ffs_use_bread / ffs_use_bwrite for the in-kernel filesystem

    g_use_g_read_data / g_use_g_write_data for kernel geom clients

    ufs_use_sa_read for the standalone code (stand/libsa/ufs.c
	but not stand/libsa/ufsread.c which is size constrained)

    use_pread / use_pwrite for libufs

Uses of these interfaces are in the UFS filesystem, geoms journal &
label, libsa changes, and libufs. They also permeate out into the
filesystem utilities fsck_ffs, newfs, growfs, clri, dump, quotacheck,
fsirand, fstyp, and quot. Some of these utilities should probably be
converted to directly use libufs (like dumpfs was for example), but
there does not seem to be much win in doing so.

Tested by: Peter Holm (pho@)

lib/libufs/sblock.c
@@ -150,7 +150,6 @@ use_pread(void *devfd, off_t loc, void **bufp, int size)
 	int fd;
 
 	fd = *(int *)devfd;
-	free(*bufp);
 	if ((*bufp = malloc(size)) == NULL)
 		return (ENOSPC);
 	if (pread(fd, *bufp, size, loc) != size)

stand/libsa/ufs.c
@@ -518,7 +518,7 @@ ufs_open(upath, f)
 
 	/* read super block */
 	twiddle(1);
-	if ((rc = ffs_sbget(f, &fs, -1, 0, ufs_use_sa_read)) != 0)
+	if ((rc = ffs_sbget(f, &fs, -1, "stand", ufs_use_sa_read)) != 0)
 		goto out;
 	fp->f_fs = fs;
 	/*
@@ -688,7 +688,6 @@ ufs_use_sa_read(void *devfd, off_t loc, void **bufp, int size)
 	int error;
 
 	f = (struct open_file *)devfd;
-	free(*bufp);
 	if ((*bufp = malloc(size)) == NULL)
 		return (ENOSPC);
 	error = (f->f_dev->dv_strategy)(f->f_devdata, F_READ, loc / DEV_BSIZE,

sys/geom/geom_io.c
@@ -957,6 +957,9 @@ g_use_g_read_data(void *devfd, off_t loc, void **bufp, int size)
 {
 	struct g_consumer *cp;
 
+	KASSERT(*bufp == NULL,
+	    ("g_use_g_read_data: non-NULL *bufp %p\n", *bufp));
+
 	cp = (struct g_consumer *)devfd;
 	/*
 	 * Take care not to issue an invalid I/O request. The offset of
@@ -966,8 +969,6 @@ g_use_g_read_data(void *devfd, off_t loc, void **bufp, int size)
 	 */
 	if (loc % cp->provider->sectorsize != 0)
 		return (ENOENT);
-	if (*bufp != NULL)
-		g_free(*bufp);
 	*bufp = g_read_data(cp, loc, size, NULL);
 	if (*bufp == NULL)
 		return (ENOENT);

sys/geom/journal/g_journal_ufs.c
@@ -72,11 +72,11 @@ g_journal_ufs_dirty(struct g_consumer *cp)
 
 	fs = NULL;
 	if (SBLOCKSIZE % cp->provider->sectorsize != 0 ||
-	    ffs_sbget(cp, &fs, -1, NULL, g_use_g_read_data) != 0) {
+	    ffs_sbget(cp, &fs, -1, M_GEOM, g_use_g_read_data) != 0) {
 		GJ_DEBUG(0, "Cannot find superblock to mark file system %s "
 		    "as dirty.", cp->provider->name);
-		if (fs != NULL)
-			g_free(fs);
+		KASSERT(fs == NULL,
+		    ("g_journal_ufs_dirty: non-NULL fs %p\n", fs));
 		return;
 	}
 	GJ_DEBUG(0, "clean=%d flags=0x%x", fs->fs_clean, fs->fs_flags);

sys/geom/label/g_label_ufs.c
@@ -77,9 +77,9 @@ g_label_ufs_taste_common(struct g_consumer *cp, char *label, size_t size, int wh
 
 	fs = NULL;
 	if (SBLOCKSIZE % pp->sectorsize != 0 ||
-	    ffs_sbget(cp, &fs, -1, NULL, g_use_g_read_data) != 0) {
-		if (fs != NULL)
-			g_free(fs);
+	    ffs_sbget(cp, &fs, -1, M_GEOM, g_use_g_read_data) != 0) {
+		KASSERT(fs == NULL,
+		    ("g_label_ufs_taste_common: non-NULL fs %p\n",fs);
 		return;
 	}
 

sys/ufs/ffs/ffs_subr.c
@@ -151,6 +151,7 @@ static int readsuper(void *, struct fs **, off_t, int,
  * superblock. Memory is allocated for the superblock by the readfunc and
  * is returned. If filltype is non-NULL, additional memory is allocated
  * of type filltype and filled in with the superblock summary information.
+ * All memory is freed when any error is returned.
  *
  * If a superblock is found, zero is returned. Otherwise one of the
  * following error values is returned:
@@ -172,16 +173,24 @@ ffs_sbget(void *devfd, struct fs **fsp, off_t altsblock,
 	int32_t *lp;
 	char *buf;
 
+	fs = NULL;
 	*fsp = NULL;
 	if (altsblock != -1) {
-		if ((error = readsuper(devfd, fsp, altsblock, 1,
-		     readfunc)) != 0)
+		if ((error = readsuper(devfd, &fs, altsblock, 1,
+		     readfunc)) != 0) {
+			if (fs != NULL)
+				UFS_FREE(fs, filltype);
 			return (error);
+		}
 	} else {
 		for (i = 0; sblock_try[i] != -1; i++) {
-			if ((error = readsuper(devfd, fsp, sblock_try[i], 0,
+			if ((error = readsuper(devfd, &fs, sblock_try[i], 0,
 			     readfunc)) == 0)
 				break;
+			if (fs != NULL) {
+				UFS_FREE(fs, filltype);
+				fs = NULL;
+			}
 			if (error == ENOENT)
 				continue;
 			return (error);
@@ -190,20 +199,18 @@ ffs_sbget(void *devfd, struct fs **fsp, off_t altsblock,
 			return (ENOENT);
 	}
 	/*
-	 * If not filling in summary information, return.
-	 */
-	if (filltype == NULL)
-		return (0);
-	/*
 	 * Read in the superblock summary information.
 	 */
-	fs = *fsp;
 	size = fs->fs_cssize;
 	blks = howmany(size, fs->fs_fsize);
 	if (fs->fs_contigsumsize > 0)
 		size += fs->fs_ncg * sizeof(int32_t);
 	size += fs->fs_ncg * sizeof(u_int8_t);
-	space = UFS_MALLOC(size, filltype, M_WAITOK);
+	/* When running in libufs or libsa, UFS_MALLOC may fail */
+	if ((space = UFS_MALLOC(size, filltype, M_WAITOK)) == NULL) {
+		UFS_FREE(fs, filltype);
+		return (ENOSPC);
+	}
 	fs->fs_csp = (struct csum *)space;
 	for (i = 0; i < blks; i += fs->fs_frag) {
 		size = fs->fs_bsize;
@@ -213,9 +220,10 @@ ffs_sbget(void *devfd, struct fs **fsp, off_t altsblock,
 		error = (*readfunc)(devfd,
 		    dbtob(fsbtodb(fs, fs->fs_csaddr + i)), (void **)&buf, size);
 		if (error) {
-			UFS_FREE(buf, filltype);
+			if (buf != NULL)
+				UFS_FREE(buf, filltype);
 			UFS_FREE(fs->fs_csp, filltype);
-			fs->fs_csp = NULL;
+			UFS_FREE(fs, filltype);
 			return (error);
 		}
 		memcpy(space, buf, size);
@@ -231,6 +239,7 @@ ffs_sbget(void *devfd, struct fs **fsp, off_t altsblock,
 	size = fs->fs_ncg * sizeof(u_int8_t);
 	fs->fs_contigdirs = (u_int8_t *)space;
 	bzero(fs->fs_contigdirs, size);
+	*fsp = fs;
 	return (0);
 }
 
@@ -246,8 +255,6 @@ readsuper(void *devfd, struct fs **fsp, off_t sblockloc, int isaltsblk,
 	int error;
 
 	error = (*readfunc)(devfd, sblockloc, (void **)fsp, SBLOCKSIZE);
-	if (*fsp != NULL)
-		(*fsp)->fs_csp = NULL;	/* Not yet any summary information */
 	if (error != 0)
 		return (error);
 	fs = *fsp;
@@ -263,6 +270,8 @@ readsuper(void *devfd, struct fs **fsp, off_t sblockloc, int isaltsblk,
 	    fs->fs_bsize >= roundup(sizeof(struct fs), DEV_BSIZE)) {
 		/* Have to set for old filesystems that predate this field */
 		fs->fs_sblockactualloc = sblockloc;
+		/* Not yet any summary information */
+		fs->fs_csp = NULL;
 		return (0);
 	}
 	return (ENOENT);

sys/ufs/ffs/ffs_vfsops.c
@@ -1075,14 +1075,11 @@ ffs_use_bread(void *devfd, off_t loc, void **bufp, int size)
 	struct buf *bp;
 	int error;
 
-	free(*bufp, M_UFSMNT);
+	KASSERT(*bufp == NULL, ("ffs_use_bread: non-NULL *bufp %p\n", *bufp));
 	*bufp = malloc(size, M_UFSMNT, M_WAITOK);
 	if ((error = bread((struct vnode *)devfd, btodb(loc), size, NOCRED,
-	    &bp)) != 0) {
-		free(*bufp, M_UFSMNT);
-		*bufp = NULL;
+	    &bp)) != 0)
 		return (error);
-	}
 	bcopy(bp->b_data, *bufp, size);
 	bp->b_flags |= B_INVAL | B_NOCACHE;
 	brelse(bp);