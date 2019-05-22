markjdb/freebsd

commit 23e298b9b75bf5521c84d1c80c3b3a2772ac6caa

Distinguish _CID match and _HID match and make lower priority probe when _CID match.
Reviewed by: jhb, imp
Differential Revision:https://reviews.freebsd.org/D16468


git-svn-id: svn+ssh://svn.freebsd.org/base/head@339754 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/acpica/acpi_hpet.c
@@ -450,16 +450,15 @@ hpet_identify(driver_t *driver, device_t parent)
 static int
 hpet_probe(device_t dev)
 {
-	ACPI_FUNCTION_TRACE((char *)(uintptr_t) __func__);
+	int rv;
 
+	ACPI_FUNCTION_TRACE((char *)(uintptr_t) __func__);
 	if (acpi_disabled("hpet") || acpi_hpet_disabled)
 		return (ENXIO);
-	if (acpi_get_handle(dev) != NULL &&
-	    ACPI_ID_PROBE(device_get_parent(dev), dev, hpet_ids) == NULL)
-		return (ENXIO);
-
-	device_set_desc(dev, "High Precision Event Timer");
-	return (0);
+	rv = ACPI_ID_PROBE(device_get_parent(dev), dev, hpet_ids, NULL);
+	if (rv <= 0)
+	   device_set_desc(dev, "High Precision Event Timer");
+	return (rv);
 }
 
 static int

sys/dev/acpi_support/acpi_asus.c
@@ -549,15 +549,16 @@ acpi_asus_probe(device_t dev)
 	ACPI_OBJECT		Arg, *Obj;
 	ACPI_OBJECT_LIST	Args;
 	static char		*asus_ids[] = { "ATK0100", "ASUS010", NULL };
+	int rv;
 	char *rstr;
 
 	ACPI_FUNCTION_TRACE((char *)(uintptr_t)__func__);
 
 	if (acpi_disabled("asus"))
 		return (ENXIO);
-	rstr = ACPI_ID_PROBE(device_get_parent(dev), dev, asus_ids);
-	if (rstr == NULL) {
-		return (ENXIO);
+	rv = ACPI_ID_PROBE(device_get_parent(dev), dev, asus_ids, &rstr);
+	if (rv > 0) {
+		return (rv);
 	}
 
 	sc = device_get_softc(dev);
@@ -595,7 +596,7 @@ acpi_asus_probe(device_t dev)
 			sc->model = &acpi_samsung_models[0];
 			device_set_desc(dev, "Samsung P30 Laptop Extras");
 			AcpiOsFree(Buf.Pointer);
-			return (0);
+			return (rv);
 		}
 
 		/* EeePC */
@@ -603,7 +604,7 @@ acpi_asus_probe(device_t dev)
 			sc->model = &acpi_eeepc_models[0];
 			device_set_desc(dev, "ASUS EeePC");
 			AcpiOsFree(Buf.Pointer);
-			return (0);
+			return (rv);
 		}
 	}
 
@@ -627,7 +628,7 @@ good:
 
 			sbuf_delete(sb);
 			AcpiOsFree(Buf.Pointer);
-			return (0);
+			return (rv);
 		}
 		
 		/*

sys/dev/acpi_support/acpi_fujitsu.c
@@ -228,16 +228,15 @@ acpi_fujitsu_probe(device_t dev)
 {
 	char *name;
 	char buffer[64];
+	int rv;
 
-	name = ACPI_ID_PROBE(device_get_parent(dev), dev, fujitsu_ids);
-	if (acpi_disabled("fujitsu") || name == NULL ||
-	    device_get_unit(dev) > 1)
+	rv =  ACPI_ID_PROBE(device_get_parent(dev), dev, fujitsu_ids, &name);
+	if (acpi_disabled("fujitsu") || rv > 0 || device_get_unit(dev) > 1)
 		return (ENXIO);
-
 	sprintf(buffer, "Fujitsu Function Hotkeys %s", name);
 	device_set_desc_copy(dev, buffer);
 
-	return (0);
+	return (rv);
 }
 
 static int

sys/dev/acpi_support/acpi_ibm.c
@@ -411,14 +411,17 @@ acpi_ibm_mic_led_set (struct acpi_ibm_softc *sc, int arg)
 static int
 acpi_ibm_probe(device_t dev)
 {
+	int rv;
+
 	if (acpi_disabled("ibm") ||
-	    ACPI_ID_PROBE(device_get_parent(dev), dev, ibm_ids) == NULL ||
 	    device_get_unit(dev) != 0)
 		return (ENXIO);
+	rv = ACPI_ID_PROBE(device_get_parent(dev), dev, ibm_ids, NULL);
 
-	device_set_desc(dev, "IBM ThinkPad ACPI Extras");
-
-	return (0);
+	if (rv <= 0) 
+		device_set_desc(dev, "IBM ThinkPad ACPI Extras");
+	
+	return (rv);
 }
 
 static int

sys/dev/acpi_support/acpi_panasonic.c
@@ -137,14 +137,16 @@ static int
 acpi_panasonic_probe(device_t dev)
 {
 	static char *mat_ids[] = { "MAT0019", NULL };
-
+	int rv;
+	
 	if (acpi_disabled("panasonic") ||
-	    ACPI_ID_PROBE(device_get_parent(dev), dev, mat_ids) == NULL ||
 	    device_get_unit(dev) != 0)
 		return (ENXIO);
+	rv = ACPI_ID_PROBE(device_get_parent(dev), dev, mat_ids, NULL);
 
-	device_set_desc(dev, "Panasonic Notebook Hotkeys");
-	return (0);
+	if (rv <= 0)
+		device_set_desc(dev, "Panasonic Notebook Hotkeys");
+	return (rv);
 }
 
 static int

sys/dev/acpi_support/acpi_rapidstart.c
@@ -62,14 +62,16 @@ static char    *rapidstart_ids[] = {"INT3392", NULL};
 static int
 acpi_rapidstart_probe(device_t dev)
 {
+	int rv;
+
 	if (acpi_disabled("rapidstart") ||
-	    ACPI_ID_PROBE(device_get_parent(dev), dev, rapidstart_ids) == NULL ||
 	    device_get_unit(dev) != 0)
 		return (ENXIO);
+	rv = ACPI_ID_PROBE(device_get_parent(dev), dev, rapidstart_ids, NULL);
+	if (rv <= 0)
+		device_set_desc(dev, "Intel Rapid Start ACPI device");
 
-	device_set_desc(dev, "Intel Rapid Start ACPI device");
-
-	return (0);
+	return (rv);
 	
 }
 

sys/dev/acpi_support/acpi_sony.c
@@ -114,11 +114,11 @@ static char    *sny_id[] = {"SNY5001", NULL};
 static int
 acpi_sony_probe(device_t dev)
 {
-	int ret = ENXIO;
+	int ret;
 
-	if (ACPI_ID_PROBE(device_get_parent(dev), dev, sny_id)) {
+	ret = ACPI_ID_PROBE(device_get_parent(dev), dev, sny_id, NULL);
+	if (ret <= 0) {
 		device_set_desc(dev, "Sony notebook controller");
-		ret = 0;
 	}
 	return (ret);
 }

sys/dev/acpi_support/acpi_toshiba.c
@@ -220,14 +220,15 @@ static int
 acpi_toshiba_probe(device_t dev)
 {
 	static char *tosh_ids[] = { "TOS6200", "TOS6207", "TOS6208", NULL };
+	int rv;
 
 	if (acpi_disabled("toshiba") ||
-	    ACPI_ID_PROBE(device_get_parent(dev), dev, tosh_ids) == NULL ||
 	    device_get_unit(dev) != 0)
 		return (ENXIO);
-
-	device_set_desc(dev, "Toshiba HCI Extras");
-	return (0);
+	rv = ACPI_ID_PROBE(device_get_parent(dev), dev, tosh_ids, NULL);
+	if (rv <= 0)
+		device_set_desc(dev, "Toshiba HCI Extras");
+	return (rv);
 }
 
 static int
@@ -543,15 +544,17 @@ static int
 acpi_toshiba_video_probe(device_t dev)
 {
 	static char *vid_ids[] = { "TOS6201", NULL };
+	int rv;
 
 	if (acpi_disabled("toshiba") ||
-	    ACPI_ID_PROBE(device_get_parent(dev), dev, vid_ids) == NULL ||
 	    device_get_unit(dev) != 0)
 		return (ENXIO);
 
 	device_quiet(dev);
-	device_set_desc(dev, "Toshiba Video");
-	return (0);
+	rv = ACPI_ID_PROBE(device_get_parent(dev), dev, vid_ids, NULL);
+	if (rv <= 0)
+		device_set_desc(dev, "Toshiba Video");
+	return (rv);
 }
 
 static int

commit 182c0687b9dc79e65fee932d16095d10da377b58

acpi_hpet: restore support for timers defined only in HPET table
This fixes a regress introduced in r339754.
After that change the code required that there is a HPET device
in the ACPI namespace.
The problem has been noticed on an PC Engines apu2 system.

While here, fix a small formatting issue.

sys/dev/acpica/acpi_hpet.c
@@ -455,9 +455,12 @@ hpet_probe(device_t dev)
 	ACPI_FUNCTION_TRACE((char *)(uintptr_t) __func__);
 	if (acpi_disabled("hpet") || acpi_hpet_disabled)
 		return (ENXIO);
-	rv = ACPI_ID_PROBE(device_get_parent(dev), dev, hpet_ids, NULL);
+	if (acpi_get_handle(dev) != NULL)
+		rv = ACPI_ID_PROBE(device_get_parent(dev), dev, hpet_ids, NULL);
+	else
+		rv = 0;
 	if (rv <= 0)
-	   device_set_desc(dev, "High Precision Event Timer");
+		device_set_desc(dev, "High Precision Event Timer");
 	return (rv);
 }
 