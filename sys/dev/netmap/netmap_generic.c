markjdb/freebsd

commit 138a42ca8a23300bbf2667a3111db8e89fee9ac3

netmap: fix kernel pointer printing in netmap_generic.c
Print the adapter name rather than the address of the adapter
to avoid kernel address leakage.

PR:		Bug 238642
Submitted by:	Fuqian Huang <huangfq.daxian@gmail.com>
Reviewed by:	vmaffione
MFC after:	1 week


git-svn-id: svn+ssh://svn.freebsd.org/base/head@349752 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/netmap/netmap_generic.c
@@ -1024,7 +1024,7 @@ generic_netmap_dtor(struct netmap_adapter *na)
 		         */
 		        netmap_adapter_put(prev_na);
 		}
-		nm_prinf("Native netmap adapter %p restored", prev_na);
+		nm_prinf("Native netmap adapter for %s restored", prev_na->name);
 	}
 	NM_RESTORE_NA(ifp, prev_na);
 	/*
@@ -1126,7 +1126,7 @@ generic_netmap_attach(struct ifnet *ifp)
 
 	nm_os_generic_set_features(gna);
 
-	nm_prinf("Emulated adapter for %s created (prev was %p)", na->name, gna->prev);
+	nm_prinf("Emulated adapter for %s created (prev was %s)", na->name, gna->prev->name);
 
 	return retval;
 }

commit 0306f54fced63a6d7449ead95df7bf7cbc86c10f

MFC r349966
netmap: fix bug introduced by r349752

r349752 introduced a NULL pointer reference bug
in the emulated netmap code.

Reported by:    lwhsu

sys/dev/netmap/netmap_generic.c
@@ -1126,7 +1126,8 @@ generic_netmap_attach(struct ifnet *ifp)
 
 	nm_os_generic_set_features(gna);
 
-	nm_prinf("Emulated adapter for %s created (prev was %s)", na->name, gna->prev->name);
+	nm_prinf("Emulated adapter for %s created (prev was %s)", na->name,
+	    gna->prev ? gna->prev->name : "NULL");
 
 	return retval;
 }