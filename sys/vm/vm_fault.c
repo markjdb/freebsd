markjdb/freebsd

commit 10590559925ae6245ba7d207371f1d29c31fe3f6

powerpc64: Implement Radix MMU for POWER9 CPUs
Summary:
POWER9 supports two MMU formats: traditional hashed page tables, and Radix
page tables, similar to what's presesnt on most other architectures.  The
PowerISA also specifies a process table -- a table of page table pointers--
which on the POWER9 is only available with the Radix MMU, so we can take
advantage of it with the Radix MMU driver.

Written by Matt Macy.

Differential Revision: https://reviews.freebsd.org/D19516


git-svn-id: svn+ssh://svn.freebsd.org/base/head@360887 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/vm/vm_fault.c
@@ -299,7 +299,7 @@ static int
 vm_fault_soft_fast(struct faultstate *fs)
 {
 	vm_page_t m, m_map;
-#if (defined(__aarch64__) || defined(__amd64__) || (defined(__arm__) && \
+#if (defined(__aarch64__) || defined(__amd64__) || defined(__powerpc64__) || (defined(__arm__) && \
     __ARM_ARCH >= 6) || defined(__i386__) || defined(__riscv)) && \
     VM_NRESERVLEVEL > 0
 	vm_page_t m_super;
@@ -320,7 +320,7 @@ vm_fault_soft_fast(struct faultstate *fs)
 	}
 	m_map = m;
 	psind = 0;
-#if (defined(__aarch64__) || defined(__amd64__) || (defined(__arm__) && \
+#if (defined(__aarch64__) || defined(__amd64__) || defined(__powerpc64__) || (defined(__arm__) && \
     __ARM_ARCH >= 6) || defined(__i386__) || defined(__riscv)) && \
     VM_NRESERVLEVEL > 0
 	if ((m->flags & PG_FICTITIOUS) == 0 &&

sys/conf/files.powerpc
@@ -135,6 +135,7 @@ powerpc/aim/locore.S		optional	aim no-obj
 powerpc/aim/aim_machdep.c	optional	aim
 powerpc/aim/mmu_oea.c		optional	aim powerpc
 powerpc/aim/mmu_oea64.c		optional	aim
+powerpc/aim/mmu_radix.c		optional	aim powerpc64
 powerpc/aim/moea64_if.m		optional	aim
 powerpc/aim/moea64_native.c	optional	aim
 powerpc/aim/mp_cpudep.c		optional	aim

sys/powerpc/aim/aim_machdep.c
@@ -136,6 +136,8 @@ __FBSDID("$FreeBSD$");
 struct bat	battable[16];
 #endif
 
+int radix_mmu = 0;
+
 #ifndef __powerpc64__
 /* Bits for running on 64-bit systems in 32-bit mode. */
 extern void	*testppc64, *testppc64size;
@@ -451,7 +453,14 @@ aim_cpu_init(vm_offset_t toc)
 	 * in case the platform module had a better idea of what we
 	 * should do.
 	 */
-	if (cpu_features & PPC_FEATURE_64)
+	if (cpu_features2 & PPC_FEATURE2_ARCH_3_00) {
+		radix_mmu = 0;
+		TUNABLE_INT_FETCH("radix_mmu", &radix_mmu);
+		if (radix_mmu)
+			pmap_mmu_install(MMU_TYPE_RADIX, BUS_PROBE_GENERIC);
+		else
+			pmap_mmu_install(MMU_TYPE_G5, BUS_PROBE_GENERIC);
+	} else if (cpu_features & PPC_FEATURE_64)
 		pmap_mmu_install(MMU_TYPE_G5, BUS_PROBE_GENERIC);
 	else
 		pmap_mmu_install(MMU_TYPE_OEA, BUS_PROBE_GENERIC);

sys/powerpc/aim/mmu_oea.c
@@ -322,6 +322,7 @@ void moea_dumpsys_map(mmu_t mmu, vm_paddr_t pa, size_t sz, void **va);
 void moea_scan_init(mmu_t mmu);
 vm_offset_t moea_quick_enter_page(mmu_t mmu, vm_page_t m);
 void moea_quick_remove_page(mmu_t mmu, vm_offset_t addr);
+boolean_t moea_page_is_mapped(mmu_t mmu, vm_page_t m);
 static int moea_map_user_ptr(mmu_t mmu, pmap_t pm,
     volatile const void *uaddr, void **kaddr, size_t ulen, size_t *klen);
 static int moea_decode_kernel_ptr(mmu_t mmu, vm_offset_t addr,
@@ -364,6 +365,7 @@ static mmu_method_t moea_methods[] = {
 	MMUMETHOD(mmu_page_set_memattr,	moea_page_set_memattr),
 	MMUMETHOD(mmu_quick_enter_page, moea_quick_enter_page),
 	MMUMETHOD(mmu_quick_remove_page, moea_quick_remove_page),
+	MMUMETHOD(mmu_page_is_mapped,	moea_page_is_mapped),
 
 	/* Internal interfaces */
 	MMUMETHOD(mmu_bootstrap,       	moea_bootstrap),
@@ -1102,6 +1104,12 @@ moea_quick_enter_page(mmu_t mmu, vm_page_t m)
 void
 moea_quick_remove_page(mmu_t mmu, vm_offset_t addr)
 {
+}
+
+boolean_t
+moea_page_is_mapped(mmu_t mmu, vm_page_t m)
+{
+	return (!LIST_EMPTY(&(m)->md.mdpg_pvoh));
 }
 
 /*

sys/powerpc/aim/mmu_oea64.c
@@ -120,8 +120,7 @@ uintptr_t moea64_get_unique_vsid(void);
  *
  */
 
-#define PV_LOCK_PER_DOM	(PA_LOCK_COUNT * 3)
-#define PV_LOCK_COUNT	(PV_LOCK_PER_DOM * MAXMEMDOM)
+#define PV_LOCK_COUNT	PA_LOCK_COUNT
 static struct mtx_padalign pv_lock[PV_LOCK_COUNT];
  
 /*
@@ -130,8 +129,7 @@ static struct mtx_padalign pv_lock[PV_LOCK_COUNT];
  * index at (N << 45).
  */
 #ifdef __powerpc64__
-#define PV_LOCK_IDX(pa)	(pa_index(pa) % PV_LOCK_PER_DOM + \
-			(((pa) >> 45) % MAXMEMDOM) * PV_LOCK_PER_DOM)
+#define PV_LOCK_IDX(pa)	((pa_index(pa) * (((pa) >> 45) + 1)) % PV_LOCK_COUNT)
 #else
 #define PV_LOCK_IDX(pa)	(pa_index(pa) % PV_LOCK_COUNT)
 #endif
@@ -305,6 +303,7 @@ void moea64_dumpsys_map(mmu_t mmu, vm_paddr_t pa, size_t sz,
 void moea64_scan_init(mmu_t mmu);
 vm_offset_t moea64_quick_enter_page(mmu_t mmu, vm_page_t m);
 void moea64_quick_remove_page(mmu_t mmu, vm_offset_t addr);
+boolean_t moea64_page_is_mapped(mmu_t mmu, vm_page_t m);
 static int moea64_map_user_ptr(mmu_t mmu, pmap_t pm,
     volatile const void *uaddr, void **kaddr, size_t ulen, size_t *klen);
 static int moea64_decode_kernel_ptr(mmu_t mmu, vm_offset_t addr,
@@ -353,6 +352,7 @@ static mmu_method_t moea64_methods[] = {
 	MMUMETHOD(mmu_page_set_memattr,	moea64_page_set_memattr),
 	MMUMETHOD(mmu_quick_enter_page, moea64_quick_enter_page),
 	MMUMETHOD(mmu_quick_remove_page, moea64_quick_remove_page),
+	MMUMETHOD(mmu_page_is_mapped,	moea64_page_is_mapped),
 #ifdef __powerpc64__
 	MMUMETHOD(mmu_page_array_startup,	moea64_page_array_startup),
 #endif
@@ -1423,6 +1423,12 @@ moea64_quick_remove_page(mmu_t mmu, vm_offset_t addr)
 	    ("moea64_quick_remove_page: invalid address"));
 	mtx_unlock(PCPU_PTR(aim.qmap_lock));
 	sched_unpin();	
+}
+
+boolean_t
+moea64_page_is_mapped(mmu_t mmu, vm_page_t m)
+{
+	return (!LIST_EMPTY(&(m)->md.mdpg_pvoh));
 }
 
 /*

sys/powerpc/booke/pmap.c
@@ -356,6 +356,7 @@ static int		mmu_booke_map_user_ptr(mmu_t mmu, pmap_t pm,
 static int		mmu_booke_decode_kernel_ptr(mmu_t mmu, vm_offset_t addr,
     int *is_user, vm_offset_t *decoded_addr);
 static void		mmu_booke_page_array_startup(mmu_t , long);
+static boolean_t mmu_booke_page_is_mapped(mmu_t mmu, vm_page_t m);
 
 
 static mmu_method_t mmu_booke_methods[] = {
@@ -398,6 +399,7 @@ static mmu_method_t mmu_booke_methods[] = {
 	MMUMETHOD(mmu_quick_enter_page, mmu_booke_quick_enter_page),
 	MMUMETHOD(mmu_quick_remove_page, mmu_booke_quick_remove_page),
 	MMUMETHOD(mmu_page_array_startup,	mmu_booke_page_array_startup),
+	MMUMETHOD(mmu_page_is_mapped,	mmu_booke_page_is_mapped),
 
 	/* Internal interfaces */
 	MMUMETHOD(mmu_bootstrap,	mmu_booke_bootstrap),
@@ -1247,6 +1249,13 @@ mmu_booke_decode_kernel_ptr(mmu_t mmu, vm_offset_t addr, int *is_user,
 
 	*decoded_addr = addr;
 	return (0);
+}
+
+static boolean_t
+mmu_booke_page_is_mapped(mmu_t mmu, vm_page_t m)
+{
+
+	return (!TAILQ_EMPTY(&(m)->md.pv_list));
 }
 
 /*

sys/powerpc/include/cpufunc.h
@@ -185,6 +185,34 @@ powerpc_sync(void)
 	__asm __volatile ("sync" : : : "memory");
 }
 
+static __inline int
+cntlzd(uint64_t word)
+{
+	uint64_t result;
+	/* cntlzd %0, %1 */
+	__asm __volatile(".long 0x7c000074 |  (%1 << 21) | (%0 << 16)" :
+	    "=r"(result) : "r"(word));
+
+	return (int)result;
+}
+
+static __inline int
+cnttzd(uint64_t word)
+{
+	uint64_t result;
+	/* cnttzd %0, %1 */
+	__asm __volatile(".long 0x7c000474 |  (%1 << 21) | (%0 << 16)" :
+	    "=r"(result) : "r"(word));
+
+	return (int)result;
+}
+
+static __inline void
+ptesync(void)
+{
+	__asm __volatile("ptesync");
+}
+
 static __inline register_t
 intr_disable(void)
 {

commit 11a0aa036176565007b3a243e036a0731cddfdab

Properly sort ifdef archs in vm_fault_soft_fast superpage guards.
Sort broken in r360887.

sys/vm/vm_fault.c
@@ -299,9 +299,9 @@ static int
 vm_fault_soft_fast(struct faultstate *fs)
 {
 	vm_page_t m, m_map;
-#if (defined(__aarch64__) || defined(__amd64__) || defined(__powerpc64__) || (defined(__arm__) && \
-    __ARM_ARCH >= 6) || defined(__i386__) || defined(__riscv)) && \
-    VM_NRESERVLEVEL > 0
+#if (defined(__aarch64__) || defined(__amd64__) || (defined(__arm__) && \
+    __ARM_ARCH >= 6) || defined(__i386__) || defined(__powerpc64__) || \
+    defined(__riscv)) && VM_NRESERVLEVEL > 0
 	vm_page_t m_super;
 	int flags;
 #endif
@@ -320,9 +320,9 @@ vm_fault_soft_fast(struct faultstate *fs)
 	}
 	m_map = m;
 	psind = 0;
-#if (defined(__aarch64__) || defined(__amd64__) || defined(__powerpc64__) || (defined(__arm__) && \
-    __ARM_ARCH >= 6) || defined(__i386__) || defined(__riscv)) && \
-    VM_NRESERVLEVEL > 0
+#if (defined(__aarch64__) || defined(__amd64__) || (defined(__arm__) && \
+    __ARM_ARCH >= 6) || defined(__i386__) || defined(__powerpc64__) || \
+    defined(__riscv)) && VM_NRESERVLEVEL > 0
 	if ((m->flags & PG_FICTITIOUS) == 0 &&
 	    (m_super = vm_reserv_to_superpage(m)) != NULL &&
 	    rounddown2(vaddr, pagesizes[m_super->psind]) >= fs->entry->start &&