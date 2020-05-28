markjdb/freebsd

commit 04e3c4cb00747ad28923e1ce5e788f4588ae4432

Make NFS address selection use fib4_lookup().
fib4_lookup_nh_ represents pre-epoch generation of fib api,
providing less guarantees over pointer validness and requiring
on-stack data copying.
Switch call to use new fib4_lookup(), allowing to eventually
deprecate old api.

Differential Revision:	https://reviews.freebsd.org/D24977


git-svn-id: https://svn.freebsd.org/base/head@361575 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/fs/nfsclient/nfs_clport.c
@@ -53,6 +53,7 @@ __FBSDID("$FreeBSD$");
 #include <netinet/if_ether.h>
 #include <netinet6/ip6_var.h>
 #include <net/if_types.h>
+#include <net/route/nhop.h>
 
 #include <fs/nfsclient/nfs_kdtrace.h>
 
@@ -976,24 +977,29 @@ nfscl_getmyip(struct nfsmount *nmp, struct in6_addr *paddr, int *isinet6p)
 #endif
 #ifdef INET
 	if (nmp->nm_nam->sa_family == AF_INET) {
+		struct epoch_tracker et;
+		struct nhop_object *nh;
 		struct sockaddr_in *sin;
-		struct nhop4_extended nh_ext;
+		struct in_addr addr = {};
 
 		sin = (struct sockaddr_in *)nmp->nm_nam;
+		NET_EPOCH_ENTER(et);
 		CURVNET_SET(CRED_TO_VNET(nmp->nm_sockreq.nr_cred));
-		error = fib4_lookup_nh_ext(fibnum, sin->sin_addr, 0, 0,
-		    &nh_ext);
+		nh = fib4_lookup(fibnum, sin->sin_addr, 0, NHR_NONE, 0);
 		CURVNET_RESTORE();
-		if (error != 0)
+		if (nh != NULL)
+			addr = IA_SIN(ifatoia(nh->nh_ifa))->sin_addr;
+		NET_EPOCH_EXIT(et);
+		if (nh == NULL)
 			return (NULL);
 
-		if (IN_LOOPBACK(ntohl(nh_ext.nh_src.s_addr))) {
+		if (IN_LOOPBACK(ntohl(addr.s_addr))) {
 			/* Ignore loopback addresses */
 			return (NULL);
 		}
 
 		*isinet6p = 0;
-		*((struct in_addr *)paddr) = nh_ext.nh_src;
+		*((struct in_addr *)paddr) = addr;
 
 		return (u_int8_t *)paddr;
 	}

commit 2c3526da648e7c9197fe9e60ed613b550071e02f

Fix NOINET6 build broken by r361575.
Reported by:	ci, hps

sys/fs/nfsclient/nfs_clport.c
@@ -971,7 +971,7 @@ u_int8_t *
 nfscl_getmyip(struct nfsmount *nmp, struct in6_addr *paddr, int *isinet6p)
 {
 #if defined(INET6) || defined(INET)
-	int error, fibnum;
+	int fibnum;
 
 	fibnum = curthread->td_proc->p_fibnum;
 #endif
@@ -1007,6 +1007,7 @@ nfscl_getmyip(struct nfsmount *nmp, struct in6_addr *paddr, int *isinet6p)
 #ifdef INET6
 	if (nmp->nm_nam->sa_family == AF_INET6) {
 		struct sockaddr_in6 *sin6;
+		int error;
 
 		sin6 = (struct sockaddr_in6 *)nmp->nm_nam;
 