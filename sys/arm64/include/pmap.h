d28a485132eaba7de497f3b361b6cdf61d22f322
Andrew Turner <andrew@FreeBSD.org>
2022-04-29 10:30:38 +0100

    Disable promotion on pcpu memory on arm64
    We need to be careful to not promote or demote the memory containing
    the per-CPU structures as the exception handlers will dereference it
    so any time it's invalid may cause recursive exceptions.
    
    Add a new pmap function to set a flag in the pte marking memory that
    cannot be promoted or demoted and use it to mark pcpu memory.
    
    Sponsored by:	The FreeBSD Foundation
    Differential Revision: https://reviews.freebsd.org/D35434
    
    (cherry picked from commit e3917bb256dea63945b5bef7fe2b792a280fcafe)

--- sys/arm64/include/pmap.h
+++ sys/arm64/include/pmap.h
@@ -155,6 +155,7 @@ void	pmap_kremove_device(vm_offset_t, vm_size_t);
 void	*pmap_mapdev_attr(vm_offset_t pa, vm_size_t size, vm_memattr_t ma);
 bool	pmap_page_is_mapped(vm_page_t m);
 bool	pmap_ps_enabled(pmap_t pmap);
+void	pmap_disable_promotion(vm_offset_t sva, vm_size_t size);
 
 void	*pmap_mapdev(vm_offset_t, vm_size_t);
 void	*pmap_mapbios(vm_paddr_t, vm_size_t);
