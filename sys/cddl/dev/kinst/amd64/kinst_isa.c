7faa5d5f144dbb86b7c205c9420acc79ba25ef4c
Christos Margiolis <christos@margiolis.net>
2023-04-28 18:12:14 +0300

    kinst(4): minor changes

--- sys/cddl/dev/kinst/amd64/kinst_isa.c
+++ sys/cddl/dev/kinst/amd64/kinst_isa.c
@@ -17,7 +17,6 @@
 
 #include "kinst.h"
 
-#define KINST_PUSHL_RBP		0x55
 #define KINST_STI		0xfb
 #define KINST_POPF		0x9d
 
@@ -606,7 +605,7 @@ kinst_md_deinit(void)
 	CPU_FOREACH(cpu) {
 		tramp = DPCPU_ID_GET(cpu, intr_tramp);
 		if (tramp != NULL) {
-			kinst_trampoline_dealloc(DPCPU_ID_GET(cpu, intr_tramp));
+			kinst_trampoline_dealloc(tramp);
 			DPCPU_ID_SET(cpu, intr_tramp, NULL);
 		}
 	}
