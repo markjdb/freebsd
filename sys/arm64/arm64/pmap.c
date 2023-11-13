914d847d606678d975219f28afa88cd2cea4a963
Andrew Turner <andrew@FreeBSD.org>
2023-11-13 16:34:56 +0000

    arm64: Use pmap_early_vtophys in pmap_bootstrap_san
    Use pmap_early_vtophys to find the physical address of the kernel base
    rather than using the calculated offset as it will be removed in a
    latter commit.
    
    Sponsored by:	Arm Ltd
    Differential Revision:	https://reviews.freebsd.org/D42566
    
    (cherry picked from commit 5fae53589eacbf65855419922c881b3a25a68e03)

--- sys/arm64/arm64/pmap.c
+++ sys/arm64/arm64/pmap.c
@@ -1069,10 +1069,13 @@ pmap_bootstrap(vm_offset_t l0pt, vm_offset_t l1pt, vm_paddr_t kernstart,
  * - Map that entire range using L2 superpages.
  */
 void
-pmap_bootstrap_san(vm_paddr_t kernstart)
+pmap_bootstrap_san(void)
 {
 	vm_offset_t va;
+	vm_paddr_t kernstart;
 	int i, shadow_npages, nkasan_l2;
+
+	kernstart = pmap_early_vtophys(KERNBASE);
 
 	/*
 	 * Rebuild physmap one more time, we may have excluded more regions from
