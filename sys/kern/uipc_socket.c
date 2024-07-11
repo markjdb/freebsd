07b90c857166045dc35f29db37e528e91bd3d2f7
Drew Gallatin <gallatin@netflix.com>
2024-07-11 13:10:38 -0400

    splice: Allow splicing between all TCP sessions
    Compare the proto number, rather than the protosw entry pointer
    so as to allow splicing v4 and v6 connections together.
    
    JIRA: NCD-6637

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -1592,7 +1592,7 @@ so_splice(struct socket *so, struct socket *so2, struct splice *splice)
 		return (EINVAL);
 	/* Handle only TCP for now; TODO: other streaming protos */
 	if (so->so_proto->pr_protocol != IPPROTO_TCP ||
-	    so2->so_proto != so->so_proto)
+	    so2->so_proto->pr_protocol != IPPROTO_TCP)
 		return (EPROTONOSUPPORT);
 	if (so->so_vnet != so2->so_vnet)
 		return (EINVAL);
