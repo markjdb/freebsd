3755d5d0465cc0539defb41b1022cbe52714d314
Mark Johnston <markj@FreeBSD.org>
2025-02-17 16:08:02 +0000

    umtx: Fix a bug in do_lock_pp()
    If the lock is unowned (i.e., owner == UMUTEX_CONTESTED), we might get a
    spurious failure, and in that case we need to retry the loop.

--- sys/kern/kern_umtx.c
+++ sys/kern/kern_umtx.c
@@ -2616,6 +2616,10 @@ do_lock_pp(struct thread *td, struct umutex *m, uint32_t flags,
 			}
 		} else if (owner == UMUTEX_RB_NOTRECOV) {
 			error = ENOTRECOVERABLE;
+		} else if (owner == UMUTEX_CONTESTED) {
+			/* Spurious failure, retry. */
+			umtxq_unbusy_unlocked(&uq->uq_key);
+			continue;
 		}
 
 		if (try != 0)
@@ -2825,6 +2829,10 @@ do_set_ceiling(struct thread *td, struct umutex *m, uint32_t ceiling,
 		} else if (owner == UMUTEX_RB_NOTRECOV) {
 			error = ENOTRECOVERABLE;
 			break;
+		} else if (owner == UMUTEX_CONTESTED) {
+			/* Spurious failure, retry. */
+			umtxq_unbusy_unlocked(&uq->uq_key);
+			continue;
 		}
 
 		/*
@@ -2833,6 +2841,7 @@ do_set_ceiling(struct thread *td, struct umutex *m, uint32_t ceiling,
 		 */
 		if (error != 0)
 			break;
+
 
 		/*
 		 * We set the contested bit, sleep. Otherwise the lock changed
