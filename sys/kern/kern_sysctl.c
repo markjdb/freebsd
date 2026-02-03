markjdb/freebsd

commit 406da392ef8d77b39fe9bfd36bdb440eb3e5ae0a

sysctl(9): Booleans: Accept integers to ease knob conversion
In sysctl_handle_bool(), if the output buffer (for the old value) has
room for exactly 4 bytes (sizeof(int)), then output the current boolean
value as an integer rather than a 'uint8_t'.  Conversely, if 4 bytes
exactly remain in the input buffer (for the new value), treat them as an
integer and derive the new boolean value from it.

Doing so allows to convert existing integer syscstl knobs that are
interpreted as a boolean into true boolean ones while staying
backwards-compatible.

That brings no drawback as no code currently uses sysctl_handle_bool()
as part of a series of calls to sysctl_handle_*() functions for
(de)serialization of some compound structure.  If that case ever
materializes, it can be easily solved, e.g., by creating
a sysctl_handle_bool_strict() variant.

In the future, we might want to go further and generally be more liberal
in the external type of integers we accept and output, by tolerating any
kind of supported integers (8-bit to 64-bit), enabling integer type
changes of knob's internal representations without breaking the ABI for
consumers hardcoding the passed integers (instead of relying on sysctl
knob type information).

Reviewed by:    jhb
MFC after:      2 weeks
Sponsored by:   The FreeBSD Foundation
Differential Revision:  https://reviews.freebsd.org/D54925

sys/kern/kern_sysctl.c
@@ -1619,8 +1619,8 @@ static SYSCTL_NODE(_sysctl, CTL_SYSCTL_OIDLABEL, oidlabel, CTLFLAG_RD |
 int
 sysctl_handle_bool(SYSCTL_HANDLER_ARGS)
 {
-	uint8_t temp;
 	int error;
+	uint8_t temp;
 
 	/*
 	 * Attempt to get a coherent snapshot by making a copy of the data.
@@ -1630,16 +1630,44 @@ sysctl_handle_bool(SYSCTL_HANDLER_ARGS)
 	else
 		temp = arg2 ? 1 : 0;
 
-	error = SYSCTL_OUT(req, &temp, sizeof(temp));
+	/*
+	 * In order to support backwards-compatible conversion of integer knobs
+	 * that are used as booleans to true boolean knobs, whose internal state
+	 * is stored as a 'bool' and not an 'int', if exactly 4 bytes remain in
+	 * the output buffer, we assume that the caller expected an 'int'
+	 * instead of a 'uint8_t'.
+	 */
+	if (req->oldidx >= req->oldlen)
+		return (ENOMEM);
+	if (req->oldlen - req->oldidx == sizeof(int)) {
+		int temp_int = temp;
+
+		error = SYSCTL_OUT(req, &temp_int, sizeof(temp_int));
+	} else
+		error = SYSCTL_OUT(req, &temp, sizeof(temp));
 	if (error || !req->newptr)
 		return (error);
 
 	if (!arg1)
 		error = EPERM;
 	else {
-		error = SYSCTL_IN(req, &temp, sizeof(temp));
-		if (!error)
-			*(bool *)arg1 = temp ? 1 : 0;
+		/*
+		 * Conversely, if the input buffer has exactly 4 bytes to read,
+		 * use them all to produce a bool.
+		 */
+		if (req->newidx >= req->newlen)
+			return (ENOMEM);
+		if (req->newlen - req->newidx == sizeof(int)) {
+			int temp_int;
+
+			error = SYSCTL_IN(req, &temp_int, sizeof(temp_int));
+			if (error == 0)
+				*(bool *)arg1 = temp_int != 0 ? 1 : 0;
+		} else {
+			error = SYSCTL_IN(req, &temp, sizeof(temp));
+			if (error == 0)
+				*(bool *)arg1 = temp != 0 ? 1 : 0;
+		}
 	}
 	return (error);
 }

commit 895e1c6567d9561c86f8d20b47e924911bce989e

sysctl(9): Booleans: Fix old value length discovery
When calling sysctl(3) with a null 'oldp', i.e., length discovery mode,
'oldix' can be equal to 'oldlen', and we should not fail.

More generally, let SYSCTL_OUT() and SYSCTL_IN() handle corner cases,
simply removing the comparisons between 'oldidx' and 'oldlen' and
'newidx' and 'newlen' done by hand as the test just after is an equality
that does not require to know if 'idx' is smaller than 'len'.

PR:             292917
Reported by:    cy
Fixes:          406da392ef8d ("sysctl(9): Booleans: Accept integers to ease knob conversion")
Sponsored by:   The FreeBSD Foundation

sys/kern/kern_sysctl.c
@@ -1637,8 +1637,6 @@ sysctl_handle_bool(SYSCTL_HANDLER_ARGS)
 	 * the output buffer, we assume that the caller expected an 'int'
 	 * instead of a 'uint8_t'.
 	 */
-	if (req->oldidx >= req->oldlen)
-		return (ENOMEM);
 	if (req->oldlen - req->oldidx == sizeof(int)) {
 		int temp_int = temp;
 
@@ -1655,8 +1653,6 @@ sysctl_handle_bool(SYSCTL_HANDLER_ARGS)
 		 * Conversely, if the input buffer has exactly 4 bytes to read,
 		 * use them all to produce a bool.
 		 */
-		if (req->newidx >= req->newlen)
-			return (ENOMEM);
 		if (req->newlen - req->newidx == sizeof(int)) {
 			int temp_int;
 