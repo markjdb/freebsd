markjdb/freebsd

commit 1a446f765d9cb34e8137ffa6dbb71ce551560cc1

sys/sysctl.h: Make it self-contained when included from the kernel
MFC after:      2 weeks
Sponsored by:   The FreeBSD Foundation

sys/sys/sysctl.h
@@ -36,7 +36,8 @@
 #define	_SYS_SYSCTL_H_
 
 #ifdef _KERNEL
-#include <sys/cdefs.h>
+#include <sys/types.h>
+#include <sys/_null.h>
 #include <sys/queue.h>
 #include <sys/tree.h>
 #endif

commit bfd50a32ff26067552220c6f08e6025be3603962

sys/sysctl.h: Add missing <sys/kassert.h> include
Hopefully, 'sys/sysctl.h' is really self-contained now.

This fixes the GENERIC-NODEBUG compilation error in 'hwpstate_common.c'
after commit 804329587508 (see below).

Fixes:          1a446f765d9c ("sys/sysctl.h: Make it self-contained when included from the kernel")
Fixes:          804329587508 ("hwpstate{_amd,intel}(4): Move common knobs to a separate file")
Sponsored by:   The FreeBSD Foundation

sys/sys/sysctl.h
@@ -38,6 +38,7 @@
 #ifdef _KERNEL
 #include <sys/types.h>
 #include <sys/_null.h>
+#include <sys/kassert.h>
 #include <sys/queue.h>
 #include <sys/tree.h>
 #endif